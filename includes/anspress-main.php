c33e622ec71180ba8988011258ea4bd1fa91a0ca
rahularyan <rah12@live.com>
2014-11-20 07:57:56 +0530

    Added shortlink

--- includes/anspress-main.php
+++ includes/anspress-main.php
@@ -98,9 +98,10 @@ class anspress {
 			echo '<link rel="alternate" type="application/rss+xml" title="'.__('Answers feed', 'ap').'" href="'.$a_feed.'" />';
 		}	
 		
-		if(is_question())	
-			echo '<link rel="canonical" href="'.get_permalink(get_question_id()).'"> ';
-		
+		if(is_question()){	
+			echo '<link rel="canonical" href="'.get_permalink(get_question_id()).'">';
+			echo '<link rel="shortlink" href="'.wp_get_shortlink(get_question_id()).'" />';
+		}
 	}
 
 	/**
