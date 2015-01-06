9fd2f5fa76cf72a747e7c03f96601494a634de59
rahularyan <rah12@live.com>
2015-01-06 17:47:49 +0530

    Fixed notify styles and re-coding of AnsPress JS

--- assets/anspress_site.js
+++ assets/anspress_site.js
@@ -27,9 +27,14 @@ AnsPress.site.prototype = {
 	initialize: function(){
 		ApSite = this;
 		this.ajax_id = new Object();
+		this.loading = new Object();
+		this.errors;
+		this.ajaxData;
 
 		this.doAction();
 		this.afterAjaxComplete();
+		this.appendFormError();
+		this.appendMessageBox();
 	},
 
 	doAjax: function(query, success, context, before, abort){
@@ -39,20 +44,38 @@ AnsPress.site.prototype = {
 		before 		= typeof before !== 'undefined' ? before : false;
 		abort 		= typeof abort !== 'undefined' ? abort : false;
 
-		if(abort && (typeof ApSite.ajax_id[query['ap_ajax_action']] !== 'undefined')){
-			ApSite.ajax_id[query['ap_ajax_action']].abort();
+		var action = apGetValueFromStr(query, 'ap_ajax_action');
+
+		if(abort && (typeof ApSite.ajax_id[action] !== 'undefined')){
+			ApSite.ajax_id[action].abort();
 		}
+
+		if(typeof ApSite.loading[action] !== 'undefined')
+			ApSite.hideLoading(ApSite.loading[action]);
+
+		var new_success = function new_success(data){
+			ApSite.hideLoading(ApSite.loading[action]);
+			if(success !== false)
+				success(data);
+		};
+
+		var new_before = function new_before(){
+			ApSite.loading[action] = ApSite.showLoading(context);			
+
+			if(before !== false)
+				before();
+		};
 
 		var req = $.ajax({
 					type: 'POST',
 					url: ajaxurl, 
 					data: query, 
-					beforeSend: before, 
-					success: success, 
+					beforeSend: new_before, 
+					success: new_success, 
 					dataType: 'json',
 					context:context,
 				});
-		ApSite.ajax_id[query['ap_ajax_action']] = req;
+		ApSite.ajax_id[action] = req;
 
 		return req;
 	},
@@ -76,13 +99,18 @@ AnsPress.site.prototype = {
 	 * @since 2.0
 	 */
 	afterAjaxComplete:function(){
-		var self = this;
 		$( document ).ajaxComplete(function( event, data, settings ) {
-			//console.log(data);
-			if(typeof data !== 'undefined' && typeof data.responseJSON !== 'undefined' && typeof data.responseJSON.message !== 'undefined'){
+
+			if(typeof data !== 'undefined' && typeof data.responseJSON !== 'undefined' && typeof data.responseJSON.ap_responce !== 'undefined'){
+
 				var type = typeof data.responseJSON.message_type === 'undefined' ? 'success' : data.responseJSON.message_type;
-				self.addMessage(data.responseJSON.message, type);
-				$('document').trigger('ap_after_ajax', data);
+				
+				ApSite.addMessage(data.responseJSON.message, type);
+
+				$(document).trigger('ap_after_ajax', data.responseJSON);
+				console.log(data.responseJSON.do !== 'undefined' && typeof ApSite[data.responseJSON.do] === 'function');
+				if (data.responseJSON.do !== 'undefined' && typeof ApSite[data.responseJSON.do] === 'function')
+					ApSite[data.responseJSON.do](data.responseJSON);
 			}
 		});
 	},
@@ -96,51 +124,119 @@ AnsPress.site.prototype = {
 		var offset = $(elm).offset();
 		var top = offset.top;
 		var left = offset.left;
-		
-		if($(elm).is('a')){
-			var el = $('<div class="ap-loading-icon icon-sync ap-uid loadin_in_btn" data-uid="'+ uid +'"></div>');
+
+		if($(elm).is('a') || $(elm).is('button') || $(elm).is('input[type="submit"]')){
+			var el = $('<div class="ap-loading-icon ap-uid loadin_in_btn" id="apuid-'+ uid +'"><i class="icon-sync"></i></div>');
 			$(elm).append(el);
+		}else if($(elm).is('form')){
+			var elm = $(elm).find('[type="submit"]');
+			var offset = $(elm).offset();
+			var top = offset.top;
+			var left = offset.left;
+			var el = $('<div class="ap-loading-icon fade-bg ap-uid" id="apuid-'+ uid +'"><i class="icon-sync"></i></div>');
+			$('body').append(el);
+			$(el).css({'top':top, 'left': left, 'width': $(elm).outerWidth(), 'height': $(elm).outerHeight() });
 		}else if($(elm).is('input[type="text"]')){
-			var top = top + ($(elm).outerHeight()/2) - 10;
+			var top = top + ($(elm).outerHeight()/2);
 			var left = left + $(elm).outerWidth() -30;
-			var el = $('<div class="ap-loading-icon icon-sync ap-uid" data-uid="'+ uid +'"></div>');
+			var el = $('<div class="ap-loading-icon ap-uid" id="apuid-'+ uid +'"><i class="icon-sync"></i></div>');
 			$('body').append(el);
 			$(el).css({'top':top, 'left': left});
 		}else {
 			var top = top + 6;
 			var left = left - 15;
-			var el = $('<div class="ap-loading-icon icon-sync ap-uid" data-uid="'+ uid +'"></div>');
+			var el = $('<div class="ap-loading-icon icon-sync ap-uid" id="apuid-'+ uid +'"></div>');
 			$('body').append(el);			
 			$(el).css({'top':top, 'left': left});
 		}
-		return el;
+		return '#apuid-'+ uid;
+	},
+
+	hideLoading: function(id){
+		$(id).remove();
 	},
 
 	suggest_similar_questions: function(elm){
+		var uid = this.uniqueId();
 		$(elm).on('keyup', function(){
-			var aj = ApSite.doAjax( 
+			ApSite.doAjax( 
 				apAjaxData('ap_ajax_action=suggest_similar_questions&value=' + $(elm).val()), 
 				function(data){
 					if(typeof data['html'] !== 'undefined')
 						$('#similar_suggestions').html(data['html']);
 				}, 
 				elm,
-				function(){
-					ApSite.showLoading(elm);
-				},
+				false,
 				true
 			);
 		});
 
+	},
+
+	ap_ajax_form:function(form){
+		$(form).on('submit', function(){
+			if(typeof tinyMCE !== 'undefined')
+				tinyMCE.triggerSave();
+
+			ApSite.doAjax( 
+				apAjaxData($(form).formSerialize()), 
+				function(data){
+					console.log(data);
+				}, 
+				form
+			);
+			return false;
+		})
+	},
+
+	appendFormError: function(){
+		$(document).on('ap_after_ajax', function(e, data){
+			if(typeof data.errors !== 'undefined'){
+				ApSite.clearFormErrors(data.form);
+				$.each(data.errors, function(i, message) {
+					var parent = $('#' + data.form ).find('#'+i).closest('.ap-form-fields');
+					parent.addClass('ap-have-error');
+					ApSite.helpBlock(parent, message);
+				});
+			}
+		});
+	},
+
+	helpBlock:function(elm, message){
+		/* remove existing help block */
+		if($(elm).find('.ap-form-error-message').length > 0)
+			$(elm).find('.ap-form-error-message').remove();
+			
+		$(elm).append('<p class="ap-form-error-message">'+message+'</p>');
+	},
+
+	clearFormErrors: function(form){
+		var elm = $('#' + form ).find('.ap-have-error');
+		elm.find('.ap-form-error-message').remove();
+		elm.removeClass('ap-have-error');		
+	},
+
+	appendMessageBox: function(){
+		if($('#ap-notify').length == '0')
+			$('body').append('<div id="ap-notify"></div>');
+	},
+	addMessage: function(message, type){
+		var icon = aplang[type];
+		$('<div class="ap-notify-item '+type+'"><i class="'+icon+'"></i>'+message+'</div>').appendTo('#ap-notify').delay(5000).slideUp(200);
+	},
+
+	redirect:function(data){
+		if(typeof data.redirect_to !== 'undefined')
+			window.location.replace(data.redirect_to);
 	}
+
 
 }
 
 })(jQuery)
 
 function apAjaxData(param){
-	var param = apQueryStringToJSON(param);
-	param['action'] = 'ap_ajax';
+	param = param + '&action=ap_ajax';
 	return param;
 }
 
@@ -151,8 +247,15 @@ function apQueryStringToJSON(string) {
     var result = {};
     pairs.forEach(function(pair) {
         pair = pair.split('=');
-        result[pair[0]] = decodeURIComponent(pair[1] || '');
+        result[pair[0]] = encodeURIComponent(pair[1] || '');
     });
 
     return JSON.parse(JSON.stringify(result));
+}
+
+function apGetValueFromStr(q, name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(q);
+    return results == null ? false : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
