ae1c8d279e886a9c25ed44a2f8b6d21530f6cc0e
rahularyan <rah12@live.com>
2015-01-24 07:54:29 +0530

    [removed] ap-site.js

--- theme/default/functions.php
+++ theme/default/functions.php
@@ -19,10 +19,8 @@ add_action('wp_enqueue_scripts', 'init_scripts_front', 11);
 function init_scripts_front(){
 	//if(is_anspress()){
 		wp_enqueue_script( 'jquery');				
-		wp_enqueue_script( 'jquery-form', array('jquery'), false, true );
-			
+		wp_enqueue_script( 'jquery-form', array('jquery'), false, true );			
 		wp_enqueue_script( 'ap-functions-js', ANSPRESS_URL.'assets/ap-functions.js', 'jquery');		
-		wp_enqueue_script( 'ap-site-js', ANSPRESS_URL.'assets/ap-site.js', 'jquery', AP_VERSION);		
 		wp_enqueue_script( 'anspress_acript', ANSPRESS_URL.'assets/anspress_site.js', 'jquery', AP_VERSION);		
 		wp_enqueue_script( 'tooltipster', ap_get_theme_url('js/jquery.tooltipster.min.js'), 'jquery', AP_VERSION);
 		wp_enqueue_script( 'ap-js', ap_get_theme_url('js/ap.js'), 'jquery', AP_VERSION);
