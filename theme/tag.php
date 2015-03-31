51156a14f87ccfc8f02b9d9bde0ddbfeaaeca997
rahularyan <rah12@live.com>
2015-03-31 22:41:09 +0530

    removed unused option

--- theme/tag.php
+++ theme/tag.php
@@ -1,7 +1,7 @@
 
 <div id="ap-lists" class="clearfix">
 	<div class="ap-taxo-detail">
-		<h2 class="entry-title"><?php echo $question_tag->name; ?> <span class="ap-tax-item-count"><?php printf( _n('1 Question', '%s Questions', $question_tag->count, 'ap'),  $question_tag->count); ?></span></h2>
+		<h2 class="entry-title"><?php printf(__('Question tag: %s','ap'), $question_tag->name); ?> <span class="ap-tax-item-count"><?php printf( _n('1 Question', '%s Questions', $question_tag->count, 'ap'),  $question_tag->count); ?></span></h2>
 		<?php if($question_tag->description !=''): ?>
 			<p class="ap-taxo-description"><?php echo $question_tag->description; ?></p>
 		<?php endif; ?>
