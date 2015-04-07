ea8c3c11f43991239351c45bafe4eb466ec3cf52
rahularyan <rah12@live.com>
2015-04-07 09:14:50 +0530

    [Added] <?php do_action( 'wordpress_social_login' ); ?>

--- theme/default/answer-form.php
+++ theme/default/answer-form.php
@@ -6,6 +6,7 @@
 <?php elseif( !is_user_logged_in() ): ?>
 	<div class="ap-please-login">
 		<?php printf(__('Please %s or %s to ask a question.', 'ap'), '<a href="'.wp_login_url(get_permalink()).'">'.__('Login', 'ap').'</a>', '<a href="'.wp_registration_url().'">'.__('Register', 'ap').'</a>') ?>
+		<?php do_action( 'wordpress_social_login' ); ?>
 	</div>
 <?php endif; ?>
 
--- theme/default/ask.php
+++ theme/default/ask.php
@@ -15,6 +15,7 @@
 	<?php else: ?>
 		<div class="ap-please-login">
 			<?php printf(__('Please %s or %s to ask a question.', 'ap'), '<a href="'.wp_login_url(get_permalink()).'">'.__('Login', 'ap').'</a>', '<a href="'.wp_registration_url().'">'.__('Register', 'ap').'</a>') ?>
+			<?php do_action( 'wordpress_social_login' ); ?>
 		</div>
 	<?php endif; ?>	
 </div>
