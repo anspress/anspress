f28ddc8350ae28f15ab9542402a3ee19a0581746
rahularyan <rah12@live.com>
2015-04-16 21:15:26 +0530

    [fix] Shows login message when user dont have permission to ask or answer

--- theme/default/answer-form.php
+++ theme/default/answer-form.php
@@ -3,7 +3,11 @@
 		<h3 class="ap-widget-title"><?php _e('Your answer', 'ap') ?></h3>
 		<?php ap_answer_form(get_question_id()); ?>
 	</div>
-<?php elseif( !is_user_logged_in() ): ?>
+<?php elseif (is_user_logged_in()): ?>
+	<div class="ap-no-permission">
+		<?php _e('You dont have permission to ask question.', 'ap'); ?>
+	</div>
+<?php else: ?>
 	<div class="ap-please-login">
 		<?php printf(__('Please %s or %s to ask a question.', 'ap'), '<a href="'.wp_login_url(get_permalink()).'">'.__('Login', 'ap').'</a>', '<a href="'.wp_registration_url().'">'.__('Register', 'ap').'</a>') ?>
 		<?php do_action( 'wordpress_social_login' ); ?>
--- theme/default/ask.php
+++ theme/default/ask.php
@@ -12,6 +12,10 @@
 <div id="ap-ask-page" class="clearfix">	
 	<?php if (ap_user_can_ask()): ?>
 		<?php ap_ask_form(); ?>
+	<?php elseif (is_user_logged_in()): ?>
+		<div class="ap-no-permission">
+			<?php _e('You dont have permission to ask question.', 'ap'); ?>
+		</div>
 	<?php else: ?>
 		<div class="ap-please-login">
 			<?php printf(__('Please %s or %s to ask a question.', 'ap'), '<a href="'.wp_login_url(get_permalink()).'">'.__('Login', 'ap').'</a>', '<a href="'.wp_registration_url().'">'.__('Register', 'ap').'</a>') ?>
