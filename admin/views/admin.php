620b6e8e20e0ef0407f5d99b5e80a4f351dec386
rahularyan <rah12@live.com>
2015-05-17 11:47:25 +0530

    Updated dashboard

--- admin/views/admin.php
+++ admin/views/admin.php
@@ -22,7 +22,7 @@ $settings = ap_opt();
  */
 ?>
 
-<div class="wrap">
+<div id="anspress" class="wrap">
 	<?php screen_icon(); echo '<h2>' . __( 'AnsPress Options' ) . '</h2>';
 	// This shows the page's name and an icon if one has been provided ?>
 			
@@ -30,12 +30,30 @@ $settings = ap_opt();
 		<div class="updated fade"><p><strong><?php _e( 'AnsPress options updated', 'ap' ); ?></strong></p></div>
 	<?php endif; // If the form has just been submitted, this shows the notification ?>
 	
-	<div class="anspress-options">
-		<div class="option-nav-tab clearfix">
-			<?php ap_options_nav(); ?>
+	<div class="ap-wrap">
+		<div class="anspress-options ap-wrap-left">
+			<div class="option-nav-tab clearfix">
+				<?php ap_options_nav(); ?>
+			</div>
+			<div class="ap-group-options">
+				<?php ap_option_group_fields(); ?>
+			</div>
 		</div>
-		<div class="ap-group-options">
-			<?php ap_option_group_fields(); ?>
+		<div class="ap-wrap-side">
+			<div class="ap-dash-tile clearfix">
+				<div class="anspress-links">
+					<a href="http://anspress.io/" target="_blank"><i class="apicon-anspress-icon"></i><?php _e('About AnsPress', 'ap'); ?></a>
+					<a href="http://anspress.io/questions/" target="_blank"><i class="apicon-question"></i><?php _e('Support Q&A', 'ap'); ?></a>
+					<a href="http://anspress.io/themes/" target="_blank"><i class="apicon-info"></i><?php _e('AnsPress Themes', 'ap'); ?></a>
+					<a href="http://github.com/anspress/anspress" target="_blank"><i class="apicon-mark-github"></i><?php _e('Github Repo', 'ap'); ?></a>
+				</div>
+				<div class="ap-ext-notice">
+					<i class="apicon-puzzle"></i>
+					<h3>AnsPress extensions</h3>
+					<p>Extend AnsPress capabilities, get free extensions</p>
+					<a href="http://anspress.io/extensions/" target="_blank">Browse</a>
+				</div>
+			</div>
 		</div>
 	</div>
 	
