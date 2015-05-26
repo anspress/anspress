4279a07925ba84e236e2693d6304ee095d995fc3
rahularyan <rah12@live.com>
2015-05-26 19:44:29 +0530

    [theme] Login popup improved

--- theme/default/answer-form.php
+++ theme/default/answer-form.php
@@ -31,24 +31,9 @@
 	<div class="ap-no-permission">
 		<?php _e('You dont have permission to answer this question.', 'ap'); ?>
 	</div>
-<?php else: ?>
-	<div class="ap-please-login">
-		<?php printf(__('Please %s or %s to answer this question.', 'ap'), '<a data-action="ap_modal" data-toggle="#ap-login-modal" href="'.wp_login_url(get_permalink()).'">'.__('Login', 'ap').'</a>', '<a href="'.wp_registration_url().'">'.__('Sign up', 'ap').'</a>') ?>
-		<?php do_action( 'wordpress_social_login' ); ?>
-	</div>
-	<div id="ap-login-modal" class="ap-modal">
-		<div class="ap-modal-backdrop"></div>
-		<div class="ap-modal-inner">
-			<div class="ap-modal-header">
-				<i class="ap-modal-close" data-action="ap_modal_close">&times;</i>
-				<h3 class="ap-modal-title"><?php _e('Login', 'ap'); ?></h3>
-			</div>
-			<div class="ap-modal-body">
-				<?php wp_login_form(); ?>
-			</div>
-		</div>
-	</div>
 <?php endif; ?>
+
+<?php ap_get_template_part('login-signup'); ?>
 
 
 
--- theme/default/ask.php
+++ theme/default/ask.php
@@ -16,10 +16,6 @@
 		<div class="ap-no-permission">
 			<?php _e('You dont have permission to ask question.', 'ap'); ?>
 		</div>
-	<?php else: ?>
-		<div class="ap-please-login">
-			<?php printf(__('Please %s or %s to ask a question.', 'ap'), '<a href="'.wp_login_url(get_permalink()).'">'.__('Login', 'ap').'</a>', '<a href="'.wp_registration_url().'">'.__('Register', 'ap').'</a>') ?>
-			<?php do_action( 'wordpress_social_login' ); ?>
-		</div>
-	<?php endif; ?>	
+	<?php endif; ?>
+	<?php ap_get_template_part('login-signup'); ?>
 </div>
