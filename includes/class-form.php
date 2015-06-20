690a96ce0434c88ae1cbf0873ff43fa21f5bcacb
rahularyan <rah12@live.com>
2015-06-20 16:30:53 +0530

    Error message on description field is overlapped by upload field

--- includes/class-form.php
+++ includes/class-form.php
@@ -511,55 +511,55 @@ class AnsPress_Form {
                 switch ($field['type']) {
 
                     case 'text':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->text_field($field, 'text');
                         $this->output .= '</div>';
                         break;
 
                     case 'password':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->text_field($field, 'password');
                         $this->output .= '</div>';
                         break;
 
                     case 'number':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->number_field($field);
                         $this->output .= '</div>';
                         break;
 
                     case 'checkbox':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->checkbox_field($field);
                         $this->output .= '</div>';
                         break;
 
                     case 'select':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->select_field($field);
                         $this->output .= '</div>';
                         break;
 
                     case 'taxonomy_select':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->taxonomy_select_field($field);
                         $this->output .= '</div>';
                         break;
 
                     case 'page_select':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->page_select_field($field);
                         $this->output .= '</div>';
                         break;
 
                     case 'textarea':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->textarea_field($field);
                         $this->output .= '</div>';
                         break;
 
                     case 'editor':
-                        $this->output .= '<div class="ap-form-fields'.$error_class.'">';
+                        $this->output .= '<div class="ap-field-'.@$field['name'].' ap-form-fields'.$error_class.'">';
                         $this->editor_field($field);
                         $this->output .= '</div>';
                         break;
