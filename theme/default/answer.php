6bc6e25e31c60e46360c631dd9f517f017feaa75
rahularyan <rah12@live.com>
2015-07-18 15:34:12 +0530

    Added responsive styles for question page

--- theme/default/answer.php
+++ theme/default/answer.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Answer content
- * 	
+ *
  * @author Rahul Aryan <support@anspress.io>
  * @link http://anspress.io/anspress
  * @since 0.1
@@ -17,23 +17,23 @@ if(!ap_answer_user_can_view()){
 <div id="answer_<?php the_ID(); ?>" <?php post_class() ?> data-id="<?php the_ID(); ?>" data-index="<?php echo @$i; ?>" itemprop="suggestedAnswer<?php echo ap_answer_is_best() ? ' acceptedAnswer' : ''; ?>" itemtype="http://schema.org/Answer" itemscope="">
 	<div class="ap-content">
 		<div class="ap-single-vote"><?php ap_answer_the_vote_button(); ?></div>
-		<div class="ap-avatar ap-pull-left">
+		<div class="ap-avatar">
 			<a href="<?php ap_answer_the_author_link(); ?>"<?php ap_hover_card_attributes(ap_answer_get_author_id()); ?>>
 				<?php ap_answer_the_author_avatar(); ?>
-			</a>		
+			</a>
 		</div>
 		<div class="ap-a-cells clearfix">
-			<div class="ap-q-metas">			
+			<div class="ap-q-metas">
 				<?php ap_user_display_meta(true, false, true); ?>
 				<?php ap_answer_the_time(); ?>
 			</div>
-			<div class="ap-q-inner">			
+			<div class="ap-q-inner">
 				<div class="ap-answer-content ap-q-content" itemprop="text">
 					<?php the_content(); ?>
 				</div>
 				<?php ap_answer_the_active_time(); ?>
-				<?php ap_post_status_description(ap_answer_get_the_answer_id()) ?>			
-				<?php ap_post_actions_buttons() ?>			
+				<?php ap_post_status_description(ap_answer_get_the_answer_id()) ?>
+				<?php ap_post_actions_buttons() ?>
 			</div>
 			<?php ap_answer_the_comments(); ?>
 		</div>
--- theme/default/question.php
+++ theme/default/question.php
@@ -8,38 +8,38 @@
  * @author    Rahul Aryan <support@anspress.io>
  */
 ?>
-<div id="ap-single" class="ap-q clearfix" itemtype="http://schema.org/Question" itemscope="">	
-	<div class="ap-question-lr row">		
+<div id="ap-single" class="ap-q clearfix" itemtype="http://schema.org/Question" itemscope="">
+	<div class="ap-question-lr row">
 		<div class="ap-q-left <?php echo (is_active_sidebar( 'ap-qsidebar' ) || ap_opt('show_question_sidebar')) ? 'col-md-8' : 'col-md-12'; ?>">
-			<div class="ap-question-meta clearfix">					
+			<div class="ap-question-meta clearfix">
 				<?php echo ap_display_question_metas() ?>
 			</div>
 			<div id="question" role="main" class="ap-content question" data-id="<?php ap_question_the_ID(); ?>">
-				<div class="ap-single-vote"><?php ap_question_the_vote_button(); ?></div>			
-				<?php 
+				<div class="ap-single-vote"><?php ap_question_the_vote_button(); ?></div>
+				<?php
 					/**
 					 * ACTION: ap_before_question_title
 					 * @since 	2.0
 					 */
 					do_action('ap_before_question_content');
-				?>				
-				<div class="ap-avatar ap-pull-left">
+				?>
+				<div class="ap-avatar">
 					<a href="<?php ap_question_the_author_link(); ?>"<?php ap_hover_card_attributes(ap_question_get_author_id()); ?>>
 						<?php ap_question_the_author_avatar( ap_opt('avatar_size_qquestion') ); ?>
-					</a>						
+					</a>
 				</div>
 				<div class="ap-q-cells clearfix">
 					<div class="ap-q-metas">
 						<?php ap_user_display_meta(true, false, true); ?>
 						<span><?php ap_question_the_time(); ?></span>
 					</div>
-					
+
 					<!-- Start ap-content-inner -->
 					<div class="ap-q-inner">
 						<div class="question-content ap-q-content" itemprop="text">
-							<?php the_content(); ?>									
+							<?php the_content(); ?>
 						</div>
-						<?php 
+						<?php
 							/**
 							 * ACTION: ap_after_question_content
 							 * @since 	2.0
@@ -47,7 +47,7 @@
 							do_action('ap_after_question_content');
 						?>
 
-						<?php 
+						<?php
 							/**
 							 * ACTION: ap_after_question_content
 							 * @since 	2.0.0-alpha2
@@ -59,7 +59,7 @@
 						<?php ap_post_status_description(ap_question_get_the_ID());	?>
 						<?php ap_post_actions_buttons() ?>
 
-						<?php 
+						<?php
 							/**
 							 * ACTION: ap_after_question_actions
 							 * @since 	2.0
@@ -67,17 +67,17 @@
 							do_action('ap_after_question_actions');
 						?>
 					</div>
-					<!-- End ap-content-inner -->					
+					<!-- End ap-content-inner -->
 					<?php ap_question_the_comments(); ?>
-				</div>		
+				</div>
 			</div>
-			
-			<?php 
+
+			<?php
 				/**
 				 * Output list of answers
 				 */
 				ap_question_the_answers();
-			?>				
+			?>
 			<?php ap_question_the_answer_form(); ?>
 		</div>
 		<?php if ( is_active_sidebar( 'ap-qsidebar' ) || ap_opt('show_question_sidebar')){ ?>
