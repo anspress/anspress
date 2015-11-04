6ca7a4e08aa0d651e12d5f02dc1e93c2bb5292e2
rahularyan <rah12@live.com>
2015-11-04 10:06:27 +0530

    Deprecated notice in related question widget

--- widgets/related_questions.php
+++ widgets/related_questions.php
@@ -1,9 +1,28 @@
 <?php
+/**
+ * AnsPress related question widget.	
+ * Shows related questions based on tags
+ *
+ * @link 	https://anspress.io
+ * @since 	2.4
+ * @author  Rahul Aryan <support@anspress.io>
+ * @package AnsPress/AP_Related_questions
+ */
+
+/**
+ * Extend WP_Widget.
+ */
 class AP_Related_questions extends WP_Widget {
 
-	public function AP_Related_questions() {
-		// Instantiate the parent object
-		parent::__construct( false, '(AnsPress) Related Questions', array('description' => 'For showing related question. This widget will only work in question page.') );
+	/**
+	 * Initialize the class
+	 */
+	public function __construct() {
+		parent::__construct(
+			'ap_related_questions',
+			__( '(AnsPress) Related Questions', 'ap' ),
+			array( 'description' => __( 'For showing related question. This widget will only work in question page.', 'ap' ) )
+		);
 	}
 
 	public function widget( $args, $instance ) {
@@ -12,36 +31,37 @@ class AP_Related_questions extends WP_Widget {
 		$title 			= apply_filters( 'widget_title', $instance['title'] );
 
 		echo $args['before_widget'];
-		if ( ! empty( $title ) )
-			echo $args['before_title'] . $title . $args['after_title'];
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title']; }
 
 		echo '<div class="ap-widget-inner">';
-		if(!class_exists('Tags_For_AnsPress')){
+		if ( ! class_exists( 'Tags_For_AnsPress' ) ) {
 			echo 'Tags plugin must be installed for related question. Get <a href="https://wordpress.org/plugins/tags-for-anspress">Tags for AnsPress</a>';
 			return;
 		}
-		$tags = get_the_terms(get_question_id(), 'question_tag' );
+		$tags = get_the_terms( get_question_id(), 'question_tag' );
 
 		$tags_in = array();
 
-		if($tags)
-			foreach($tags as $t)
-				$tags_in[] = $t->term_id;
+		if ( $tags ) {
+			foreach ( $tags as $t ) {
+				$tags_in[] = $t->term_id; }
+		}
 
-		$question_args=array(
+		$question_args = array(
 			'tax_query' 	=> array(
 				array(
 					'taxonomy' => 'question_tag',
 					'field'    => 'term_id',
 					'terms'    => $tags_in,
-				)
+				),
 			),
 			'showposts' 	=> 5,
-			'post__not_in' 	=> array(get_question_id()),
+			'post__not_in' 	=> array( get_question_id() ),
 		);
 
-		$questions = ap_get_questions($question_args);
-		include ap_get_theme_location('widget-related_questions.php');
+		$questions = ap_get_questions( $question_args );
+		include ap_get_theme_location( 'widget-related_questions.php' );
 		wp_reset_postdata();
 		echo '</div>';
 		echo $args['after_widget'];
@@ -51,13 +71,13 @@ class AP_Related_questions extends WP_Widget {
 	public function form( $instance ) {
 		$title 			= __( 'Related Questions', 'ap' );
 
-		if ( isset( $instance[ 'title' ] ) )
-			$title = $instance[ 'title' ];
+		if ( isset( $instance[ 'title' ] ) ) {
+			$title = $instance[ 'title' ]; }
 		?>
-		<p>
+        <p>
 			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>">
-		</p>
+        </p>
 
 		<?php
 	}
