25dbcba1098bfe420550f7b0ab4ad95aad27dc60
rahularyan <rah12@live.com>
2016-04-06 20:53:47 +0530

    Updated tests

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,2 +1,21 @@
 <?php
-// Here you can initialize variables that will be available to your tests
+$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
+$_SERVER['SERVER_NAME'] = '';
+$PHP_SELF = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
+
+$_tests_dir = getenv('TRAVIS_BUILD_DIR').'/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
+function _manually_load_plugin() {
+	require dirname( __FILE__ ) . '/../../anspress-question-answer.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+require $_tests_dir . '/includes/bootstrap.php';
+activate_plugin( 'anspress-question-answer/anspress-question-answer.php' );
+echo "Installing AnsPress...\n";
+
+global $current_user;
+$current_user = new WP_User(1);
+$current_user->set_role('administrator');
+wp_update_user( array( 'ID' => 1, 'first_name' => 'Admin', 'last_name' => 'User' ) );
+
--- tests/unit/test-subscribers.php
+++
@@ -1,8 +0,0 @@
-<?php
-class UserTest extends \Codeception\TestCase\Test
-{
-    public function testValidation()
-    {
-        $this->assertTrue(ap_new_subscriber( 1, 111, 'q_all', 111, 0 ));           
-    }
-}
