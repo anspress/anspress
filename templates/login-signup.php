7d555ef536726a6511a5c3c0257c96fb1ffc93ee
Rahul Aryan <rah12@live.com>
2016-12-24 09:30:04 +0530

    Improved login/signup template

--- templates/login-signup.php
+++ templates/login-signup.php
@@ -1,18 +1,26 @@
-<?php if(!is_user_logged_in() && !ap_opt('allow_anonymous')): ?>
-	<div class="ap-please-login">
-		<?php printf(__('Please %s or %s.', 'anspress-question-answer'), '<a data-action="ap_modal" data-toggle="#ap-login-modal" href="'.wp_login_url(get_permalink()).'">'.__('Login', 'anspress-question-answer').'</a>', '<a href="'.wp_registration_url().'">'.__('Sign up', 'anspress-question-answer').'</a>') ?>
-		<?php do_action( 'wordpress_social_login' ); ?>
-	</div>
-	<div id="ap-login-modal" class="ap-modal">
-		<div class="ap-modal-backdrop"></div>
-		<div class="ap-modal-inner">
-			<div class="ap-modal-header">
-				<i class="ap-modal-close" data-action="ap_modal_close">&times;</i>
-				<h3 class="ap-modal-title"><?php _e('Login', 'anspress-question-answer'); ?></h3>
-			</div>
-			<div class="ap-modal-body">
-				<?php wp_login_form(); ?>
+<?php
+/**
+ * Display login signup form
+ *
+ * @package WordPress/AnsPress
+ * @author  Rahul Aryan <support@anspress.io>
+ */
+
+?>
+
+<?php if ( ! is_user_logged_in() ) : ?>
+	<div class="ap-content ap-login">
+		<div class="ap-cell">
+			<div class="ap-cell-inner">
+				<strong class="ap-login-label"><?php esc_attr_e( 'Please login or register.', 'anspress-question-answer' ); ?></strong>
+				<?php do_action( 'wordpress_social_login' ); ?>
+				<div class="ap-login-buttons">
+					<a href="<?php echo esc_url( wp_registration_url() ); ?>" class="ap-btn"><?php esc_attr_e( 'Register', 'anspress-question-answer' ); ?></a>
+					<span class="ap-login-sep"><?php esc_attr_e( 'or', 'anspress-question-answer' ); ?></span>
+					<a href="<?php echo esc_url( wp_login_url() ); ?>" class="ap-btn"><?php esc_attr_e( 'Login', 'anspress-question-answer' ); ?></a>
+				</div>
 			</div>
 		</div>
 	</div>
+
 <?php endif; ?>
