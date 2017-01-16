8865209b9c2c6a27375f07275ee7d8f7b607d5b0
Rahul Aryan <rah12@live.com>
2017-01-16 20:20:28 +0530

    Removing codeception tests as PHPUnit test will be written from scratch

--- tests/bin/selenium.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash -e
-java -jar /tmp/selenium-server-standalone-2.42.2.jar -Djava.security.egd=file:///dev/urandom switch
--- tests/bin/upload.sh
+++
@@ -1,6 +0,0 @@
-find $TRAVIS_BUILD_DIR/tests/_output/ -name "*.png" | while read file; do
-	curl \
-	  -F "format=JSON" \
-	  -F "upload=@$file" \
-  "http://uploads.im/api"
-done
