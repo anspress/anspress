dfc37452ce523f2e91cf737c1f6b443e26f744d3
Rahul Aryan <rah12@live.com>
2017-03-05 10:00:16 +0530

    Do not remove canonical link

--- includes/class-theme.php
+++ includes/class-theme.php
@@ -208,7 +208,7 @@ class AnsPress_Theme {
 			remove_action( 'wp_head', 'rsd_link' );
 			remove_action( 'wp_head', 'wlwmanifest_link' );
 			remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
-			remove_action( 'wp_head', 'rel_canonical' );
+			//remove_action( 'wp_head', 'rel_canonical' );
 			remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
 			remove_action( 'wp_head', 'feed_links_extra', 3 );
 			remove_action( 'wp_head', 'feed_links', 2 );
