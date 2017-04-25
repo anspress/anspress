f37ef6d02a15959e0a0fb5134ba431d089c24afd
Rahul Aryan <rah12@live.com>
2017-04-25 20:40:13 +0530

    Fixed wrong text domain in category template

--- templates/addons/category/category.php
+++ templates/addons/category/category.php
@@ -7,7 +7,7 @@
 	<?php dynamic_sidebar( 'ap-top' ); ?>
 	<div class="row">
 		<div id="ap-lists" class="<?php echo is_active_sidebar( 'ap-category' ) && is_anspress() ? 'ap-col-9' : 'ap-col-12' ?>">
-			
+
 			<?php if( ap_category_have_image( $question_category->term_id ) ): ?>
 				<div class="ap-category-feat" style="height: 300px;">
 					<?php ap_category_image( $question_category->term_id, 300 ); ?>
@@ -15,7 +15,7 @@
 			<?php endif; ?>
 
 			<div class="ap-taxo-detail">
-				<div class="ap-pull-left">				
+				<div class="ap-pull-left">
 					<?php ap_category_icon( $question_category->term_id ); ?>
 				</div>
 				<div class="no-overflow">
@@ -24,29 +24,36 @@
 							<?php echo $question_category->name;?>
 						</a>
 						<span class="ap-tax-count">
-							<?php printf( _n('1 Question', '%s Questions', $question_category->count, 'categories-for-anspress'),  $question_category->count); ?>
+							<?php
+								printf(
+									_n( '1 Question', '%s Questions', $question_category->count, 'anspress-question-answer' ),
+									$question_category->count
+								);
+							?>
 						</span>
 					</div>
-					
 
-					<?php if($question_category->description !=''): ?>
-						<p class="ap-taxo-description"><?php echo $question_category->description; ?></p>
+
+					<?php if ( '' !== $question_category->description ) : ?>
+						<p class="ap-taxo-description">
+							<?php echo esc_html( $question_category->description ); ?>
+						</p>
 					<?php endif; ?>
 
 					<?php
-						$sub_cat_count = count(get_term_children( $question_category->term_id, 'question_category' ));
+						$sub_cat_count = count( get_term_children( $question_category->term_id, 'question_category' ) );
 
-						if($sub_cat_count >0){
+						if ( $sub_cat_count > 0 ) {
 							echo '<div class="ap-term-sub">';
-							echo '<div class="sub-taxo-label">' .$sub_cat_count.' '.__('Sub Categories', 'categories-for-anspress') .'</div>';
-							ap_sub_category_list($question_category->term_id);
+							echo '<div class="sub-taxo-label">' . $sub_cat_count . ' ' . __( 'Sub Categories', 'anspress-question-answer' ) . '</div>';
+							ap_sub_category_list( $question_category->term_id );
 							echo '</div>';
 						}
 					?>
-				</div>				
+				</div>
 			</div><!-- close .ap-taxo-detail -->
 
-			<?php ap_get_template_part('list-head'); ?>
+			<?php ap_get_template_part( 'list-head' ); ?>
 
 			<?php if ( ap_have_questions() ) : ?>
 
@@ -54,7 +61,7 @@
 					<?php
 						/* Start the Loop */
 						while ( ap_have_questions() ) : ap_the_question();
-							include(ap_get_theme_location('question-list-item.php'));
+							include( ap_get_theme_location( 'question-list-item.php' ) );
 						endwhile;
 					?>
 				</div><!-- close .ap-questions -->
@@ -63,13 +70,13 @@
 
 			<?php else : ?>
 
-				<?php include(ap_get_theme_location('content-none.php')); ?>
+				<?php include( ap_get_theme_location( 'content-none.php' ) ); ?>
 
 			<?php endif; ?>
 
 		</div><!-- close #ap-lists -->
 
-		<?php if ( is_active_sidebar( 'ap-category' ) && is_anspress()){ ?>
+		<?php if ( is_active_sidebar( 'ap-category' ) && is_anspress() ){ ?>
 			<div class="ap-question-right ap-col-3">
 				<?php dynamic_sidebar( 'ap-category' ); ?>
 			</div>
