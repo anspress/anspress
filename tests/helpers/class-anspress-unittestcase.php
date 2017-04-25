4eada5c568787d00f08f6bd5ca9f78a05ff1c482
Rahul Aryan <rah12@live.com>
2017-04-25 19:50:10 +0530

    Added cpt

--- tests/helpers/class-anspress-unittestcase.php
+++ tests/helpers/class-anspress-unittestcase.php
@@ -4,4 +4,34 @@ class AnsPress_UnitTestCase extends WP_UnitTestCase {
 	public static function wpSetUpBeforeClass() {
 		//AP_Activate::get_instance();
 	}
+
+	public function logout() {
+		unset( $GLOBALS['current_user'] );
+		$cookies = array(AUTH_COOKIE, SECURE_AUTH_COOKIE, LOGGED_IN_COOKIE, USER_COOKIE, PASS_COOKIE);
+		foreach ( $cookies as $c )
+			unset( $_COOKIE[ $c ] );
+	}
+
+	protected function _setRole( $role ) {
+		$post = $_POST;
+		$user_id = $this->factory->user->create( array( 'role' => $role ) );
+		wp_set_current_user( $user_id );
+		$_POST = array_merge( $_POST, $post );
+	}
+
+	protected function _handleAjax($action) {
+		// Start output buffering
+		ini_set( 'implicit_flush', false );
+		ob_start();
+		// Build the request
+		$_POST['action'] = $action;
+		$_REQUEST = $_POST;
+		// Call the hooks
+		do_action( 'wp_ajax_' . $_REQUEST['action'] );
+		// Save the output
+		$buffer = ob_get_clean();
+		if ( ! empty( $buffer ) )
+			$this->_last_response = $buffer;
+		return $buffer;
+	}
 }
--- tests/tests-CPT.php
+++ tests/tests-CPT.php
@@ -1,13 +1,11 @@
 <?php
-class ActivityTest extends AnsPress_UnitTestCase
+class Cpt_Tests extends AnsPress_UnitTestCase
 {
-	private $_question;
-	private $_answer;
-	private $_comment;
 
 	public function setUp() {
 		// before
 		parent::setUp();
+		// your set up methods here
 	}
 
 	public function tearDown() {
@@ -16,19 +14,58 @@ class ActivityTest extends AnsPress_UnitTestCase
 		parent::tearDown();
 	}
 
-	public function test_get_activity() {
-		$this->assertObjectHasAttribute( 'id', $out );
-		$this->assertObjectHasAttribute( 'user_id', $out );
-		$this->assertObjectHasAttribute( 'type', $out );
-		$this->assertObjectHasAttribute( 'parent_type', $out );
-		$this->assertObjectHasAttribute( 'status', $out );
-		$this->assertObjectHasAttribute( 'content', $out );
-		$this->assertObjectHasAttribute( 'permalink', $out );
-		$this->assertObjectHasAttribute( 'question_id', $out );
-		$this->assertObjectHasAttribute( 'answer_id', $out );
-		$this->assertObjectHasAttribute( 'item_id', $out );
-		$this->assertObjectHasAttribute( 'term_ids', $out );
-		$this->assertObjectHasAttribute( 'created', $out );
-		$this->assertObjectHasAttribute( 'updated', $out );
+	public function test_question_post_type() {
+		global $wp_post_types;
+		$this->assertArrayHasKey( 'question', $wp_post_types );
+	}
+
+	public function test_question_post_type_labels() {
+		global $wp_post_types;
+		$this->assertEquals( 'Questions', $wp_post_types['question']->labels->name );
+		$this->assertEquals( 'Question', $wp_post_types['question']->labels->singular_name );
+		$this->assertEquals( 'New question', $wp_post_types['question']->labels->add_new );
+		$this->assertEquals( 'Add new question', $wp_post_types['question']->labels->add_new_item );
+		$this->assertEquals( 'Edit question', $wp_post_types['question']->labels->edit_item );
+		$this->assertEquals( 'View question', $wp_post_types['question']->labels->view_item );
+		$this->assertEquals( 'Search questions', $wp_post_types['question']->labels->search_items );
+		$this->assertEquals( 'No question found', $wp_post_types['question']->labels->not_found );
+		$this->assertEquals( 'No questions found in trash', $wp_post_types['question']->labels->not_found_in_trash );
+		$this->assertEquals( 'All questions', $wp_post_types['question']->labels->all_items );
+		$this->assertEquals( 'Questions', $wp_post_types['question']->labels->menu_name );
+		$this->assertEquals( 1, $wp_post_types['question']->publicly_queryable );
+		$this->assertEquals( 1, $wp_post_types['question']->has_archive );
+		$this->assertEquals( 'apq', $wp_post_types['question']->query_var );
+		$this->assertEquals( 'Questions', $wp_post_types['question']->label );
+	}
+
+	public function test_answer_post_type() {
+		global $wp_post_types;
+		$this->assertArrayHasKey( 'answer', $wp_post_types );
+	}
+
+	public function test_answer_post_type_labels() {
+		global $wp_post_types;
+		$this->assertEquals( 'Answers', $wp_post_types['answer']->labels->name );
+		$this->assertEquals( 'Answer', $wp_post_types['answer']->labels->singular_name );
+		$this->assertEquals( 'New answer', $wp_post_types['answer']->labels->add_new );
+		$this->assertEquals( 'Add new answer', $wp_post_types['answer']->labels->add_new_item );
+		$this->assertEquals( 'Edit answer', $wp_post_types['answer']->labels->edit_item );
+		$this->assertEquals( 'View answer', $wp_post_types['answer']->labels->view_item );
+		$this->assertEquals( 'Search answers', $wp_post_types['answer']->labels->search_items );
+		$this->assertEquals( 'No answer found', $wp_post_types['answer']->labels->not_found );
+		$this->assertEquals( 'No answer found in trash', $wp_post_types['answer']->labels->not_found_in_trash );
+		$this->assertEquals( 'All answers', $wp_post_types['answer']->labels->all_items );
+		$this->assertEquals( 'Answers', $wp_post_types['answer']->labels->menu_name );
+		$this->assertEquals( 1, $wp_post_types['answer']->publicly_queryable );
+		$this->assertEquals( 1, $wp_post_types['answer']->has_archive );
+		$this->assertEquals( 'Answers', $wp_post_types['answer']->label );
+	}
+
+	public function test_register_post_statuses() {
+		AnsPress_Post_Status::register_post_status();
+		global $wp_post_statuses;
+
+		$this->assertInternalType( 'object', $wp_post_statuses['moderate'] );
+		$this->assertInternalType( 'object', $wp_post_statuses['private_post'] );
 	}
 }
