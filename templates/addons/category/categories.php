74cff4acb77c794e97598a4baf904c8c369573b7
Rahul Aryan <rah12@live.com>
2017-10-17 20:35:56 +0530

    Improved categories page template

--- templates/addons/category/categories.php
+++ templates/addons/category/categories.php
@@ -1,12 +1,13 @@
 <?php
 /**
- * Categories page
+ * Categories page.
  *
  * Display categories page
  *
- * @link http://anspress.io
- * @since 4.0
- * @package AnsPress
+ * @link        http://anspress.io
+ * @since       4.0
+ * @package     AnsPress
+ * @subpackage  Templates
  */
 
 global $question_categories;
@@ -14,51 +15,63 @@ global $question_categories;
 
 <?php dynamic_sidebar('ap-top');?>
 
-<div class="row">
-	<div id="ap-lists" class="<?php echo is_active_sidebar( 'ap-category' ) && is_anspress() ? 'ap-col-9' : 'ap-col-12' ?>">
+<div class="ap-row">
+	<div class="<?php echo is_active_sidebar( 'ap-category' ) && is_anspress() ? 'ap-col-9' : 'ap-col-12' ?>">
 		<div id="ap-categories" class="clearfix">
 			<ul class="ap-term-category-box clearfix">
+
 				<?php foreach ( (array) $question_categories as $key => $category ) : ?>
 					<li class="clearfix">
 						<div class="ap-category-item">
 							<div class="ap-cat-img-c">
+
 								<?php ap_category_icon( $category->term_id );?>
+
 								<span class="ap-term-count">
 									<?php
 										printf(
-											_n( '%d Question', '%d Questions', $category->count, 'anspress-question-answer' ), (int) $category->count
+											_n( '%d Question', '%d Questions', $category->count, 'anspress-question-answer' ),
+											(int) $category->count
 										);
 									?>
 								</span>
+
 								<a class="ap-categories-feat" style="height:<?php echo ap_opt( 'categories_image_height' ); ?>px" href="<?php echo get_category_link( $category ); ?>">
 									<?php echo ap_get_category_image( $category->term_id, ap_opt( 'categories_image_height' ) ); ?>
 								</a>
 							</div>
 
 							<div class="ap-term-title">
-								<a class="term-title" href="<?php echo get_category_link( $category ); ?>">
-									<?php echo $category->name; ?>
+								<a class="term-title" href="<?php echo esc_url( get_category_link( $category ) ); ?>">
+									<?php echo esc_html( $category->name ); ?>
 								</a>
+
 								<?php $sub_cat_count = count( get_term_children( $category->term_id, 'question_category' ) ); ?>
+
 								<?php if ( $sub_cat_count > 0 ) : ?>
 									<span class="ap-sub-category">
 										<?php
 											printf(
-												_n( '%d Sub category', '%d Sub categories', $sub_cat_count, 'anspress-question-answer' ), (int) $sub_cat_count
+												// Translators: %d contains count of sub category.
+												_n( '%d Sub category', '%d Sub categories', (int) $sub_cat_count, 'anspress-question-answer' ),
+												(int) $sub_cat_count
 											);
 										?>
 									</span>
-								<?php endif;?>
+								<?php endif; // End if(). ?>
+
 							</div>
 
 							<?php if ( $category->description != '' ) : ?>
 								<div class="ap-taxo-description">
-									<?php echo ap_truncate_chars( $category->description, 70 ); ?>
+									<?php echo ap_truncate_chars( $category->description, 120 ); ?>
 								</div>
 							<?php endif;?>
+
 						</div>
 					</li>
-				<?php endforeach;?>
+				<?php endforeach; // End foreach(). ?>
+
 			</ul>
 		</div>
 		<?php ap_pagination();?>
