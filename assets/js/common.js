6e0903b5689bff86e9ddfaf9d108d54de17ead15
Rahul Aryan <rah12@live.com>
2017-10-25 11:14:21 +0530

    Fixed: comments modal not showing once closed

--- assets/js/common.js
+++ assets/js/common.js
@@ -161,20 +161,22 @@ window.AnsPress = _.extend({
 		}
 	},
 	removeHash: function(){
-    var scrollV, scrollH, loc = window.location;
-    if ("pushState" in history)
-      history.pushState("", document.title, loc.pathname + loc.search);
-    else {
-			// Prevent scrolling by storing the page's current scroll offset
-			scrollV = document.body.scrollTop;
-			scrollH = document.body.scrollLeft;
+		var scrollV, scrollH, loc = window.location;
+		// Prevent scrolling by storing the page's current scroll offset
+		scrollV = document.body.scrollTop;
+		scrollH = document.body.scrollLeft;
 
-			loc.hash = "";
+    if ('pushState' in history){
 
-			// Restore the scroll offset, should be flicker free
-			document.body.scrollTop = scrollV;
-			document.body.scrollLeft = scrollH;
-    }
+			history.pushState('', document.title, loc.pathname + loc.search);
+			Backbone.history.navigate('/');
+		} else {
+			loc.hash = '';
+		}
+		// Restore the scroll offset, should be flicker free
+		document.body.scrollTop = scrollV;
+		document.body.scrollLeft = scrollH;
+
 	}
 }, Backbone.Events);
 
