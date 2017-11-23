fbce86f884a9be1ceb31839746c747b9b74e1514
YORÇI <ileti@yorci.com>
2017-11-23 18:13:23 +0300

    Buddypress template slip div fix
    unless max_num_pages equals one its not closing ( #ap-bp-answers and #ap-bp-questions ) div so its crashing template schema.

--- addons/free/buddypress.php
+++ addons/free/buddypress.php
@@ -197,9 +197,13 @@ class AnsPress_BP_Hooks {
 				ap_get_template_part( 'buddypress/question-item' );
 			endwhile;
 		}
-
-		if ( $questions->max_num_pages > 1 && false === $only_posts ) {
+		
+		
+		if ( false === $only_posts ) {
 			echo '</div>';
+		}
+		
+		if ( $questions->max_num_pages > 1 && false === $only_posts ) {
 			$args = wp_json_encode( [ '__nonce' => wp_create_nonce( 'loadmore-questions' ), 'type' => 'questions', 'current' => 1, 'user_id' => bp_displayed_user_id() ] );
 			echo '<a href="#" class="ap-bp-loadmore ap-btn" ap-loadmore="' . esc_js( $args ) . '">' . esc_attr__( 'Load more questions', 'anspress-question-answer' ) .'</a>';
 		}
@@ -245,11 +249,13 @@ class AnsPress_BP_Hooks {
 				ap_get_template_part( 'buddypress/answer-item' );
 			endwhile;
 		}
-
-		if ( $answers->max_num_pages > 1 && false === $only_posts ) {
+		
+		if ( false === $only_posts ) {
 			echo '</div>';
+		}
+		
+		if ( $answers->max_num_pages > 1 && false === $only_posts ) {
 			$args = wp_json_encode( [ '__nonce' => wp_create_nonce( 'loadmore-answers' ), 'type' => 'answers', 'current' => 1, 'user_id' => bp_displayed_user_id(), 'order_by' => ap_sanitize_unslash( 'order_by', 'r' ) ] );
-
 			echo '<a href="#" class="ap-bp-loadmore ap-btn" ap-loadmore="' . esc_js( $args ) . '">' . esc_attr__( 'Load more answers', 'anspress-question-answer' ) . '</a>';
 		}
 	}
