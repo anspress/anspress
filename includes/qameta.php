23cd2f8f03efac2848670e706b081e1c5146536d
pierrebuet <pierrebuet@mac.com>
2017-12-16 21:05:11 +0100

    loop optimization
    No need to loop trough all arguments

--- includes/qameta.php
+++ includes/qameta.php
@@ -197,6 +197,7 @@ function ap_append_qameta( $post ) {
 	foreach ( ap_qameta_fields() as $fields_name => $val ) {
 		if ( ! isset( $post_arr[ $fields_name ] ) ) {
 			$exist = false;
+			break;
 		}
 	}
 
