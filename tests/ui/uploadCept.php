2d2bba750ee56b425c8b6c0ecf9fe1986ade552f
Rahul Aryan <rah12@live.com>
2018-04-02 09:37:14 +0530

    Fixed upload test

--- tests/ui/uploadCept.php
+++ tests/ui/uploadCept.php
@@ -15,14 +15,20 @@ $I->submitForm(
 $I->amOnPage( '?p=' . $ask_page );
 $I->fillField( [ 'name' => 'form_question[post_title]' ], 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.' );
 $I->fillTinyMceEditorById( 'form_question-post_content', 'Nulla vestibulum ultricies neque eu semper. Phasellus hendrerit ullamcorper est eget tincidunt.' );
-$I->click( '#mceu_10-button' );
-$image_field = $I->executeJS( 'return jQuery(tinymce.activeEditor.getElement()).attr("id") + "-images[]"' );
-codecept_debug( $image_field );
-$I->attachFile( 'input[name="' . $image_field . '"]', 'placeholder.png' );
+
+$I->wantTo( 'Check image upload' );
+$I->click( '#form_question .ap-btn-insertimage' );
+$I->waitForElementVisible( '#ap-modal-imageUpload', 10 );
+$I->attachFile( 'input[name="form_image_upload-image"]', 'placeholder.png' );
+$I->click( '#form_image_upload .ap-btn-submit' );
+$I->waitForElementNotVisible( '#ap-modal-imageUpload', 10 );
+
+$I->wantTo( 'Check image preset in editor or not' );
 $content = $I->executeJS( 'tinymce.activeEditor.save(); return tinymce.activeEditor.getContent();' );
-$I->assertContains( '{{apimage "placeholder.png"}}', $content );
+$I->assertContains( 'wp-content/uploads/anspress-temp', $content );
 $I->waitForJS( 'return jQuery.active == 0;', 1 );
-$I->click( '//*[@id="form_question"]/button' );
+$I->scrollTo( '#form_question .ap-btn-submit', 0, -50 );
+$I->click( '#form_question .ap-btn-submit' );
 $I->makeScreenshot( 'uploadImage' );
 $I->waitForJS( 'return jQuery.active == 0;', 10 );
 $I->seeElement( '#ap-single' );
