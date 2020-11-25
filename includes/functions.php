411e328ff81c0444a9b9efe1c242d16315f9a974
petrozavodsky <petrozavodsky@gmail.com>
2020-11-25 14:49:52 +0300

    Added condition for existence of index

--- includes/functions.php
+++ includes/functions.php
@@ -1032,8 +1032,10 @@ function ap_parse_search_string( $str ) {
 				}
 			}
 
-			// Remove this pair from $bits.
-			unset( $bits[ $id ] );
+            if (isset($bits[$id])) {
+                // Remove this pair from $bits.
+                unset($bits[$id]);
+            }
 		} // Not a pair, presumably reached the query.
 		else {
 
