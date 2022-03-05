2dc2b3015cac2cbaa81379859d974f808287c3cb
RayQ <ray@qstudio.us>
2022-03-05 12:08:52 +0000

    pull

--- addons/recaptcha/recaptcha/ReCaptcha/RequestMethod/CurlPost.php
+++ addons/recaptcha/recaptcha/ReCaptcha/RequestMethod/CurlPost.php
@@ -5,6 +5,7 @@
  * @ignore
  * @copyright Copyright (c) 2015, Google Inc.
  * @link      http://www.google.com/recaptcha
+ * @internal
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,21 +26,68 @@
  * THE SOFTWARE.
  */
 
-namespace ReCaptcha;
+namespace ReCaptcha\RequestMethod;
+
+use ReCaptcha\RequestMethod;
+use ReCaptcha\RequestParameters;
 
 /**
- * Method used to send the request to the service.
+ * Sends cURL request to the reCAPTCHA service.
+ * Note: this requires the cURL extension to be enabled in PHP
+ *
+ * @see http://php.net/manual/en/book.curl.php
  *
  * @ignore
  */
-interface RequestMethod {
-
+class CurlPost implements RequestMethod {
 
 	/**
-	 * Submit the request with the specified parameters.
+	 * URL to which requests are sent via cURL.
+	 *
+	 * @const string
+	 */
+	const SITE_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+
+	/**
+	 * Curl connection to the reCAPTCHA service
+	 *
+	 * @var Curl
+	 */
+	private $curl;
+
+	public function __construct( Curl $curl = null ) {
+		if ( ! is_null( $curl ) ) {
+			$this->curl = $curl;
+		} else {
+			$this->curl = new Curl();
+		}
+	}
+
+	/**
+	 * Submit the cURL request with the specified parameters.
 	 *
 	 * @param RequestParameters $params Request parameters
 	 * @return string Body of the reCAPTCHA response
 	 */
-	public function submit( RequestParameters $params);
+	public function submit( RequestParameters $params ) {
+		$handle = $this->curl->init( self::SITE_VERIFY_URL );
+
+		$options = array(
+			CURLOPT_POST           => true,
+			CURLOPT_POSTFIELDS     => $params->toQueryString(),
+			CURLOPT_HTTPHEADER     => array(
+				'Content-Type: application/x-www-form-urlencoded',
+			),
+			CURLINFO_HEADER_OUT    => false,
+			CURLOPT_HEADER         => false,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_SSL_VERIFYPEER => true,
+		);
+		$this->curl->setoptArray( $handle, $options );
+
+		$response = $this->curl->exec( $handle );
+		$this->curl->close( $handle );
+
+		return $response;
+	}
 }
--- addons/recaptcha/recaptcha/ReCaptcha/RequestMethod/Post.php
+++ addons/recaptcha/recaptcha/ReCaptcha/RequestMethod/Post.php
@@ -5,6 +5,7 @@
  * @ignore
  * @copyright Copyright (c) 2015, Google Inc.
  * @link      http://www.google.com/recaptcha
+ * @internal
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -25,21 +26,49 @@
  * THE SOFTWARE.
  */
 
-namespace ReCaptcha;
+namespace ReCaptcha\RequestMethod;
+
+use ReCaptcha\RequestMethod;
+use ReCaptcha\RequestParameters;
 
 /**
- * Method used to send the request to the service.
+ * Sends POST requests to the reCAPTCHA service.
  *
  * @ignore
  */
-interface RequestMethod {
-
+class Post implements RequestMethod {
 
 	/**
-	 * Submit the request with the specified parameters.
+	 * URL to which requests are POSTed.
+	 *
+	 * @const string
+	 */
+	const SITE_VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify';
+
+	/**
+	 * Submit the POST request with the specified parameters.
 	 *
 	 * @param RequestParameters $params Request parameters
 	 * @return string Body of the reCAPTCHA response
 	 */
-	public function submit( RequestParameters $params);
+	public function submit( RequestParameters $params ) {
+		/**
+		 * PHP 5.6.0 changed the way you specify the peer name for SSL context options.
+		 * Using "CN_name" will still work, but it will raise deprecated errors.
+		 */
+		$peer_key = version_compare( PHP_VERSION, '5.6.0', '<' ) ? 'CN_name' : 'peer_name';
+		$options  = array(
+			'http' => array(
+				'header'      => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'      => 'POST',
+				'content'     => $params->toQueryString(),
+				// Force the peer to validate (not needed in 5.6.0+, but still works)
+				'verify_peer' => true,
+				// Force the peer validation to use www.google.com
+				$peer_key     => 'www.google.com',
+			),
+		);
+		$context = stream_context_create( $options );
+		return file_get_contents( self::SITE_VERIFY_URL, false, $context );
+	}
 }
