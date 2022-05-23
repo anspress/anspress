d8ca8daea73f805eb5b69f0c205f5523dfd039c2
Rahul Aryan <rah12@live.com>
2022-05-23 12:36:56 +0000

    Removed mysql dump

--- .devcontainer/wp-setup.sh
+++ .devcontainer/wp-setup.sh
@@ -6,7 +6,7 @@ ADMIN_USER=admin
 ADMIN_PASS=password
 ADMIN_EMAIL="admin@localhost.com"
 #Space-separated list of plugin ID's to install and activate
-PLUGINS="query-monitor rewrite-rules-inspector fakerpress buddypress wp-mail-logging"
+PLUGINS="query-monitor rewrite-rules-inspector fakerpress wp-mail-logging"
 
 #Set to true to wipe out and reset your wordpress install (on next container rebuild)
 WP_RESET=true
@@ -24,25 +24,30 @@ fi
 if [ ! -f wp-config.php ]; then
     echo "Configuring";
 	cp /var/www/html/wp-content/plugins/anspress-question-answer/.devcontainer/wp-config.php /var/www/html/wp-config.php;
+
+    #Data import
+    # cd $DEVDIR/data/
+    # for f in *.sql; do
+    #     wp db import $f --path=/var/www/html
+    # done
+
+    cd /var/www/html;
+
     # wp config create --dbhost="db" --dbname="wordpress" --dbuser="wp_user" --dbpass="wp_pass" --skip-check;
     wp core install --url="http://localhost:8080" --title="$SITE_TITLE" --admin_user="$ADMIN_USER" --admin_email="$ADMIN_EMAIL" --admin_password="$ADMIN_PASS" --skip-email;
 
-    #Data import
-    cd $DEVDIR/data/
-    for f in *.sql; do
-        wp db import $f
-    done
-
-    cd /var/www/html;
+    wp rewrite structure '/%postname%/';
+    wp rewrite flush --hard;
     wp plugin install $PLUGINS --activate;
     wp plugin activate anspress-question-answer;
+    wp theme install twentytwelve --activate;
+    curl -N http://loripsum.net/api/5 | wp post generate --count=100 --post_type=question;
+    wp user generate --count=100;
 
     # cp -r plugins/* /var/www/html/wp-content/plugins
     # for p in plugins/*; do
     #     wp plugin activate $(basename $p)
     # done
-
-    wp theme install twentytwelve --activate;
 else
     echo "Already configured"
 fi
