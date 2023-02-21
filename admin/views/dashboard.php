768edfa1e28bebc89185e4692bc05729db2331a7
Bishal Napit <mebishalnapit@gmail.com>
2023-02-21 10:54:55 +0545

    Fix - Question posted date display for all questions in the AnsPress Welcome Page

--- admin/views/dashboard.php
+++ admin/views/dashboard.php
@@ -130,7 +130,7 @@ class AnsPress_Dashboard {
 						?>
 						<li>
 							<a target="_blank" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> -
-							<span class="posted"><?php the_date(); ?></span>
+							<span class="posted"><?php echo esc_html( get_the_date() ); ?></span>
 						</li>
 					<?php endwhile; ?>
 				</ul>
