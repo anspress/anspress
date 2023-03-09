b4d45b5c5ce5d4e8eaef43d67597ba30c567a7ae
Bishal Napit <mebishalnapit@gmail.com>
2023-03-09 09:09:50 +0545

    Fix - PHP Warning: Attempt to read property name on bool in /addons/tags/tags.php on line 447

--- addons/tags/tags.php
+++ addons/tags/tags.php
@@ -443,7 +443,7 @@ class Tags extends \AnsPress\Singleton {
 			$title = ap_opt( 'tags_page_title' );
 		} elseif ( is_question_tag() ) {
 			$tag_id = sanitize_title( get_query_var( 'q_tag' ) );
-			$tag = get_term_by( 'slug', $tag_id, 'question_tag' ); // @codingStandardsIgnoreLine.
+			$tag    = $tag_id ? get_term_by( 'slug', $tag_id, 'question_tag' ) : get_queried_object(); // @codingStandardsIgnoreLine.
 			$title  = $tag->name;
 		}
 
