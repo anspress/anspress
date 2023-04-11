5584d168c70e93a9594ec72ee98a69bc9a034d37
Bishal Napit <mebishalnapit@gmail.com>
2023-04-11 11:00:02 +0545

    Fix - Edit comment button does not open the modal again if closed directlyp without updating the comment

--- assets/js/common.js
+++ assets/js/common.js
@@ -370,6 +370,7 @@ _.templateSettings = {
 		clickHide: function (e) {
 			e.preventDefault();
 			this.hide();
+			apToggleLoadedClass();
 		},
 		hide: function (runCb) {
 			if (typeof runCb === 'undefined')
@@ -777,6 +778,7 @@ jQuery(document).ready(function ($) {
 			if ($lastModal.length > 0) {
 				$name = $lastModal.attr('id').replace('ap-modal-', '');
 				AnsPress.hideModal($name);
+				apToggleLoadedClass();
 			}
 		}
 	});
@@ -865,6 +867,22 @@ jQuery(document).ready(function ($) {
 		$(this).parent().remove();
 	})
 });
+
+/**
+ * Remove loaded class whenever needed.
+ */
+function apToggleLoadedClass() {
+	// For the event to be triggered once again.
+	const loadedClass = [
+		'.comment-edit',
+	];
+	jQuery.each( loadedClass, function( i, loadedClassName ) {
+		const elem = jQuery( loadedClassName );
+		if ( elem.hasClass( 'loaded' ) ) {
+			elem.removeClass( 'loaded' );
+		}
+	} );
+}
 
 window.AnsPress.Helper = {
 	toggleNextClass: function (el) {
