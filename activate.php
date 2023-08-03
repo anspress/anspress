03f78cf7d1d816932f3785f69e71cb8ebe0fe524
Rahul <rah12@live.com>
2023-08-03 04:25:12 +0000

    Fixed: issue creating table ap_reputation_events

--- activate.php
+++ activate.php
@@ -284,20 +284,18 @@ class AP_Activate {
 		global $wpdb;
 
 		$this->tables[] = 'CREATE TABLE ' . $wpdb->ap_reputation_events . ' (
-				rep_events_id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-				slug varchar(100) NOT NULL,
-				icon varchar(100) NOT NULL,
-				label varchar(100) NOT NULL,
-				description varchar(200) NOT NULL,
-				activity varchar(200) NOT NULL,
-				parent varchar(100) NOT NULL DEFAULT "",
-				points int(5) NOT NULL DEFAULT 0,
-				PRIMARY KEY  (rep_events_id),
-				UNIQUE (slug),
-				KEY slug_key (slug),
-				KEY points_key (points),
-				KEY parent_key (parent)
-			)' . $this->charset_collate . ';';
+			rep_events_id bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+			slug varchar(100) NOT NULL UNIQUE,
+			icon varchar(100) NOT NULL,
+			label varchar(100) NOT NULL,
+			description varchar(200) NOT NULL,
+			activity varchar(200) NOT NULL,
+			parent varchar(100) NOT NULL DEFAULT \'\',
+			points int(5) NOT NULL DEFAULT 0,
+			PRIMARY KEY  (rep_events_id),
+			KEY points_key (points),
+			KEY parent_key (parent)
+		)' . $this->charset_collate . ';';
 	}
 
 	/**
