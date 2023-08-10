1ad5e54c386a928938db1a066cedfb186feed9ee
Bishal Napit <mebishalnapit@gmail.com>
2023-08-10 10:24:39 +0545

    Fix - User profile page Answers tab load more answers button hide after all answers related to the user is loaded

--- addons/profile/profile.php
+++ addons/profile/profile.php
@@ -387,6 +387,9 @@ class Profile extends \AnsPress\Singleton {
 		}
 		$html = ob_get_clean();
 
+		// Pagination fix on Ajax load more event.
+		$paged = $answers->max_num_pages > $paged ? $paged : 0;
+
 		ap_ajax_json(
 			array(
 				'success' => true,
