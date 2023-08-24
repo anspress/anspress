41aad519506ca826a08114868c5f978f74437d33
Bishal Napit <mebishalnapit@gmail.com>
2023-08-24 13:27:05 +0545

    Add - PHPUnit test for setting question and answer session

--- tests/test-session.php
+++ tests/test-session.php
@@ -13,4 +13,28 @@ class Test_Session extends TestCase {
 		$class = new \ReflectionClass('AnsPress\Session');
 		$this->assertTrue($class->hasProperty('instance') && $class->getProperty('instance')->isStatic());
 	}
+
+	/**
+	 * @covers AnsPress\Session::set_question
+	 *
+	 * @return void
+	 */
+	public function testSetQuestion() {
+		$session = \AnsPress\Session::init();
+		$session->set_question( 2 );
+		$this->assertContains( 2, $session->get( 'questions' ) );
+		$this->assertNotContains( 5, $session->get( 'questions' ) );
+	}
+
+	/**
+	 * @covers AnsPress\Session::set_answer
+	 *
+	 * @return void
+	 */
+	public function testSetAnswer() {
+		$session = \AnsPress\Session::init();
+		$session->set_answer( 2 );
+		$this->assertContains( 2, $session->get( 'answers' ) );
+		$this->assertNotContains( 5, $session->get( 'answers' ) );
+	}
 }
