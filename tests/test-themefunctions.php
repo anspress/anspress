f53c33157b622b3f1f08d19fffec26f7e8cee0d4
Bishal Napit <mebishalnapit@gmail.com>
2023-08-31 13:45:04 +0545

    Add - PHPUnit test case for the ap_post_status, is_private_post, and is_post_waiting_moderation functions

--- tests/test-themefunctions.php
+++ tests/test-themefunctions.php
@@ -27,4 +27,76 @@ class TestThemeFunctions extends TestCase {
 		$this->assertEquals( '', ap_page_title() );
 	}
 
+	/**
+	 * @covers ::ap_post_status
+	 */
+	public function testAPPostStatus() {
+		$id = $this->insert_question();
+		$this->assertEquals( 'publish', ap_post_status( $id ) );
+
+		// Check for private_post post status.
+		$id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_status'  => 'private_post',
+				'post_type'    => 'question',
+			)
+		);
+		$post = get_post( $id );
+		$this->assertEquals( 'private_post', ap_post_status( $id ) );
+
+		// Check for moderate post status.
+		$id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_status'  => 'moderate',
+				'post_type'    => 'question',
+			)
+		);
+		$post = get_post( $id );
+		$this->assertEquals( 'moderate', ap_post_status( $id ) );
+	}
+
+	/**
+	 * @covers ::is_private_post
+	 */
+	public function testPrivatePost() {
+		$id = $this->insert_question();
+		$this->assertFalse( is_private_post( $id ) );
+
+		// Check for private_post post status.
+		$id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_status'  => 'private_post',
+				'post_type'    => 'question',
+			)
+		);
+		$post = get_post( $id );
+		$this->assertTrue( is_private_post( $id ) );
+	}
+
+	/**
+	 * @covers ::is_post_waiting_moderation
+	 */
+	public function testModeratePost() {
+		$id = $this->insert_question();
+		$this->assertFalse( is_post_waiting_moderation( $id ) );
+
+		// Check for moderate post status.
+		$id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_status'  => 'moderate',
+				'post_type'    => 'question',
+			)
+		);
+		$post = get_post( $id );
+		$this->assertTrue( is_post_waiting_moderation( $id ) );
+	}
+
 }
