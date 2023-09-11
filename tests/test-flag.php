c6b1d09283b7373f8e7e2278b694f7328053e556
Bishal Napit <mebishalnapit@gmail.com>
2023-09-11 15:19:12 +0545

    Add - PHPUnit test for the ap_add_flag and ap_delete_flags functions

--- tests/test-flag.php
+++ tests/test-flag.php
@@ -7,6 +7,19 @@ class TestFlag extends TestCase {
 	use AnsPress\Tests\Testcases\Common;
 
 	/**
+	 * @covers ::ap_add_flag
+	 */
+	public function testAPAddFlag() {
+		$id = $this->insert_question();
+		$this->setRole( 'subscriber' );
+		$this->assertTrue( ap_add_flag( $id ) );
+
+		$user_id = $this->factory()->user->create( array( 'role' => 'subscriber' ) );
+		wp_set_current_user( $user_id );
+		$this->assertTrue( ap_add_flag( $id, $user_id ) );
+	}
+
+	/**
 	 * @covers ::ap_is_user_flagged
 	 */
 	public function testAPIsUserFlagged() {
@@ -21,6 +34,16 @@ class TestFlag extends TestCase {
 		ap_add_flag( $id->a );
 		$this->assertTrue( ap_is_user_flagged( $id->q ) );
 		$this->assertTrue( ap_is_user_flagged( $id->a ) );
+	}
+
+	/**
+	 * @covers ::ap_delete_flags
+	 */
+	public function testap_delete_flags() {
+		$id = $this->insert_question();
+		$this->setRole( 'subscriber' );
+		$this->assertTrue( ap_add_flag( $id ) );
+		$this->assertTrue( ap_delete_flags( $id ) );
 	}
 
 }
