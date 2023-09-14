3e4141117af23bb8b404002a13d30d4270d3c562
Bishal Napit <mebishalnapit@gmail.com>
2023-09-14 14:50:01 +0545

    Add - PHPUnit test case for ap_total_flagged_count function

--- tests/test-flag.php
+++ tests/test-flag.php
@@ -46,4 +46,44 @@ class TestFlag extends TestCase {
 		$this->assertTrue( ap_delete_flags( $id ) );
 	}
 
+	/**
+	 * @covers ::ap_total_flagged_count
+	 */
+	public function testAPTotalFlaggedCount() {
+		$total_flagged_count     = ap_total_flagged_count();
+		$total_flagged_questions = $total_flagged_count['questions'];
+		$total_flagged_answers   = $total_flagged_count['answers'];
+
+		// Test for questions.
+		$this->assertEquals( 0, $total_flagged_questions->publish );
+		$this->assertEquals( 0, $total_flagged_questions->future );
+		$this->assertEquals( 0, $total_flagged_questions->draft );
+		$this->assertEquals( 0, $total_flagged_questions->pending );
+		$this->assertEquals( 0, $total_flagged_questions->private );
+		$this->assertEquals( 0, $total_flagged_questions->trash );
+		$this->assertEquals( 0, $total_flagged_questions->{'auto-draft'} );
+		$this->assertEquals( 0, $total_flagged_questions->inherit );
+		$this->assertEquals( 0, $total_flagged_questions->{'request-pending'} );
+		$this->assertEquals( 0, $total_flagged_questions->{'request-confirmed'} );
+		$this->assertEquals( 0, $total_flagged_questions->{'request-failed'} );
+		$this->assertEquals( 0, $total_flagged_questions->{'request-completed'} );
+		$this->assertEquals( 0, $total_flagged_questions->total );
+		$this->assertEquals( 0, $total_flagged_questions->moderate );
+
+		// Test for answers.
+		$this->assertEquals( 0, $total_flagged_answers->publish );
+		$this->assertEquals( 0, $total_flagged_answers->future );
+		$this->assertEquals( 0, $total_flagged_answers->draft );
+		$this->assertEquals( 0, $total_flagged_answers->pending );
+		$this->assertEquals( 0, $total_flagged_answers->private );
+		$this->assertEquals( 0, $total_flagged_answers->trash );
+		$this->assertEquals( 0, $total_flagged_answers->{'auto-draft'} );
+		$this->assertEquals( 0, $total_flagged_answers->inherit );
+		$this->assertEquals( 0, $total_flagged_answers->{'request-pending'} );
+		$this->assertEquals( 0, $total_flagged_answers->{'request-confirmed'} );
+		$this->assertEquals( 0, $total_flagged_answers->{'request-failed'} );
+		$this->assertEquals( 0, $total_flagged_answers->{'request-completed'} );
+		$this->assertEquals( 0, $total_flagged_answers->total );
+	}
+
 }
