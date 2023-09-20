aa18f84abd17601f3e8c5d7ff23aac1150831431
Bishal Napit <mebishalnapit@gmail.com>
2023-09-20 14:18:58 +0545

    Add - PHPUnit test case for the ap_get_reputation function

--- tests/test-reputation.php
+++ tests/test-reputation.php
@@ -20,9 +20,111 @@ class TestReputation extends TestCase {
 		$this->assertIsInt( ap_insert_reputation( 'answer', $id->a ) );
 		$this->assertIsInt( ap_insert_reputation( 'select_answer', $id->a ) );
 		$this->assertIsInt( ap_insert_reputation( 'best_answer', $id->a ) );
+	}
 
-		// Test for constant.
-		define( 'AP_DISABLE_INSERT_REP', true );
-		$this->assertNull( ap_insert_reputation( 'ask', $id->q ) );
+	/**
+	 * @covers ::ap_get_reputation
+	 */
+	public function testAPGetReputation() {
+		$id = $this->insert_answer();
+
+		// Test begins.
+		$this->setRole( 'subscriber' );
+		$this->assertNull( ap_get_reputation( 'ask', $id->q ) );
+		$this->assertNull( ap_get_reputation( 'answer', $id->a ) );
+		$this->assertNull( ap_get_reputation( 'best_answer', $id->a ) );
+		$this->assertNull( ap_get_reputation( 'select_answer', $id->a ) );
+		$this->assertNull( ap_get_reputation( 'received_vote_up', $id->q ) );
+		$this->assertNull( ap_get_reputation( 'received_vote_down', $id->q ) );
+		$this->assertNull( ap_get_reputation( 'given_vote_up', $id->q ) );
+		$this->assertNull( ap_get_reputation( 'given_vote_down', $id->q ) );
+
+		// After inserting reputation.
+		ap_insert_reputation( '', $id->q );
+		$this->assertNull( ap_get_reputation( '', $id->q ) );
+		ap_insert_reputation( 'ask', $id->q );
+		$get_reputation = ap_get_reputation( 'ask', $id->q );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'ask', $get_reputation->rep_event );
+		$this->assertEquals( $id->q, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
+		ap_insert_reputation( 'best_answer', $id->a );
+		$get_reputation = ap_get_reputation( 'best_answer', $id->a );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'best_answer', $get_reputation->rep_event );
+		$this->assertEquals( $id->a, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
+		ap_insert_reputation( 'best_answer', $id->a );
+		$get_reputation = ap_get_reputation( 'best_answer', $id->a );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'best_answer', $get_reputation->rep_event );
+		$this->assertEquals( $id->a, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
+		ap_insert_reputation( 'received_vote_up', $id->q );
+		$get_reputation = ap_get_reputation( 'received_vote_up', $id->q );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'received_vote_up', $get_reputation->rep_event );
+		$this->assertEquals( $id->q, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
+		ap_insert_reputation( 'received_vote_down', $id->q );
+		$get_reputation = ap_get_reputation( 'received_vote_down', $id->q );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'received_vote_down', $get_reputation->rep_event );
+		$this->assertEquals( $id->q, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
+		ap_insert_reputation( 'given_vote_up', $id->q );
+		$get_reputation = ap_get_reputation( 'given_vote_up', $id->q );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'given_vote_up', $get_reputation->rep_event );
+		$this->assertEquals( $id->q, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
+		ap_insert_reputation( 'given_vote_down', $id->q );
+		$get_reputation = ap_get_reputation( 'given_vote_down', $id->q );
+		$this->assertNotEmpty( $get_reputation );
+		$this->assertEquals( get_current_user_id(), $get_reputation->rep_user_id );
+		$this->assertEquals( 'given_vote_down', $get_reputation->rep_event );
+		$this->assertEquals( $id->q, $get_reputation->rep_ref_id );
+		$get_reputation = (array) $get_reputation;
+		$this->assertArrayHasKey( 'rep_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_user_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_event', $get_reputation );
+		$this->assertArrayHasKey( 'rep_ref_id', $get_reputation );
+		$this->assertArrayHasKey( 'rep_date', $get_reputation );
 	}
 }
