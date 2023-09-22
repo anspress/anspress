dfc706fdfd3b6725370ddf54c384087bdd363d24
Bishal Napit <mebishalnapit@gmail.com>
2023-09-22 15:13:34 +0545

    Add - PHPUnit test case for the AnsPress\Activity_Helper::hooks method

--- tests/test-activity.php
+++ tests/test-activity.php
@@ -13,4 +13,14 @@ class TestActivity extends TestCase {
 		$class = new \ReflectionClass( 'AnsPress\Activity_Helper' );
 		$this->assertTrue( $class->hasProperty( 'instance' ) && $class->getProperty( 'instance' )->isStatic() );
 	}
+
+	/**
+	 * @covers AnsPress\Activity_Helper::hooks
+	 */
+	public function testAnsPressActivityHelperHooks() {
+		$this->assertEquals( 10, has_action( 'before_delete_post', [ 'AnsPress\Activity_Helper', '_before_delete' ] ) );
+		$this->assertEquals( 10, has_action( 'delete_comment', [ 'AnsPress\Activity_Helper', '_delete_comment' ] ) );
+		$this->assertEquals( 10, has_action( 'delete_user', [ 'AnsPress\Activity_Helper', '_delete_user' ] ) );
+		$this->assertEquals( 10, has_action( 'ap_ajax_more_activities', [ 'AnsPress\Activity_Helper', '_ajax_more_activities' ] ) );
+	}
 }
