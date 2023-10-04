dd8f84f0699e4b72efcf2fa866c52858ca80054f
Bishal Napit <mebishalnapit@gmail.com>
2023-10-04 14:52:16 +0545

    Tweak - Add and include the testcase-ajax.php file

--- tests/testcases/includes.php
+++ tests/testcases/includes.php
@@ -1,4 +1,7 @@
 <?php
 
+// Ajax abstract class include.
+require_once \dirname( __FILE__ ) . '/testcase-ajax.php';
+
 // Common class file include.
 require_once \dirname( __FILE__ ) . '/common.php';
