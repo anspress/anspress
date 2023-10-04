7a29796e9729012f769397efed1503d09eef5a05
Bishal Napit <mebishalnapit@gmail.com>
2023-10-04 15:31:00 +0545

    Add - Required methods for the Ajax PHPUnit tests

---
+++ tests/testcases/ajax.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AnsPress\Tests\Testcases;
+
+trait Ajax {
+	public function ap_ajax_success( $key = false, $return_json = false ) {
+		preg_match( '#<div[^>]*>(.*?)</div>#', $this->_last_response, $match );
+		if ( ! isset( $match[1] ) ) {
+			return false;
+		}
+		$res = json_decode( $match[1] );
+		if ( false !== $return_json ) {
+			return $res;
+		}
+		if ( false !== $key ) {
+			$this->assertObjectHasAttribute( $key, $res );
+			if ( ! isset( $res->$key ) ) {
+				return false;
+			}
+			return $res->$key;
+		}
+	}
+
+	public function getAjaxResponse() {
+		return $this->ap_ajax_success( false, true );
+	}
+
+	public function handle( $action ) {
+		try {
+			$this->_handleAjax( $action );
+		} catch ( \WPAjaxDieStopException $e ) {
+			$this->_last_response = $e->getMessage();
+		}
+	}
+}
