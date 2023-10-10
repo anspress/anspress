1f4a9cde637ec12fc6334ae5b097cee774dd19cd
Bishal Napit <mebishalnapit@gmail.com>
2023-10-10 10:20:02 +0545

    Add - PHPUnit test case for the ap_question_status function

--- tests/test-qaquery.php
+++ tests/test-qaquery.php
@@ -6,6 +6,8 @@ use Yoast\WPTestUtils\WPIntegration\TestCase;
 
 class TestQAQuery extends TestCase {
 
+	use Testcases\Common;
+
 	public function testClassProperties() {
 		$class = new \ReflectionClass( 'Question_Query' );
 		$this->assertTrue( $class->hasProperty( 'count_request' ) && $class->getProperty( 'count_request' )->isPublic() );
@@ -23,5 +25,86 @@ class TestQAQuery extends TestCase {
 		$this->assertTrue( method_exists( 'Question_Query', 'reset_questions_data' ) );
 		$this->assertTrue( method_exists( 'Question_Query', 'get_ids' ) );
 		$this->assertTrue( method_exists( 'Question_Query', 'pre_fetch' ) );
+	}
+
+	/**
+	 * @covers ::ap_question_status
+	 */
+	public function testAPQuestionStatus() {
+		// Test on publish post status.
+		$id = $this->insert_question();
+		$this->assertNull( ap_question_status( $id ) );
+
+		// Test on other post statuses.
+		// Moderate post status.
+		$q1_id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_type'    => 'question',
+				'post_status'  => 'moderate',
+			)
+		);
+		ob_start();
+		ap_question_status( $q1_id );
+		$moderate_post_status = ob_get_clean();
+		$this->assertEquals( '<span class="ap-post-status moderate">Moderate</span>', $moderate_post_status );
+
+		// Private post post status.
+		$q2_id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_type'    => 'question',
+				'post_status'  => 'private_post',
+			)
+		);
+		ob_start();
+		ap_question_status( $q2_id );
+		$private_post_status = ob_get_clean();
+		$this->assertEquals( '<span class="ap-post-status private_post">Private</span>', $private_post_status );
+
+		// Future post status.
+		$q3_id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_type'    => 'question',
+				'post_status'  => 'future',
+				'post_date'    => '9999-12-31 23:59:59',
+			)
+		);
+		ob_start();
+		ap_question_status( $q3_id );
+		$future_status = ob_get_clean();
+		$this->assertEquals( '<span class="ap-post-status future">Scheduled</span>', $future_status );
+
+		// Draft post status.
+		$q4_id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_type'    => 'question',
+				'post_status'  => 'draft',
+			)
+		);
+		ob_start();
+		ap_question_status( $q4_id );
+		$draft_status = ob_get_clean();
+		$this->assertEquals( '<span class="ap-post-status draft">Draft</span>', $draft_status );
+
+		// Pending review post status.
+		$q5_id = $this->factory->post->create(
+			array(
+				'post_title'   => 'Question title',
+				'post_content' => 'Question content',
+				'post_type'    => 'question',
+				'post_status'  => 'pending',
+			)
+		);
+		ob_start();
+		ap_question_status( $q5_id );
+		$pending_status = ob_get_clean();
+		$this->assertEquals( '<span class="ap-post-status pending">Pending</span>', $pending_status );
 	}
 }
