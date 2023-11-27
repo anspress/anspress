b5aa70dee04616959c75b289b47104f3462c93ff
Bishal Napit <mebishalnapit@gmail.com>
2023-11-27 14:18:07 +0545

    Add - PHPUnit test for the query_var method inside the AnsPress_Rewrite class

--- tests/test-rewrite.php
+++ tests/test-rewrite.php
@@ -22,4 +22,30 @@ class TestRewrite extends TestCase {
 		$this->assertTrue( method_exists( 'AnsPress_Rewrite', 'add_query_var' ) );
 		$this->assertTrue( method_exists( 'AnsPress_Rewrite', 'shortlink' ) );
 	}
+
+	/**
+	 * @covers AnsPress_Rewrite::query_var
+	 */
+	public function testAnsPressRewriteQueryVar() {
+		$this->assertEquals( 10, has_filter( 'query_vars', [ 'AnsPress_Rewrite', 'query_var' ] ) );
+
+		// Test begins.
+		$anspress_rewrite = \AnsPress_Rewrite::query_var( [] );
+		$this->assertTrue( in_array( 'edit_post_id', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'ap_nonce', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'question_id', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'answer_id', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'answer', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'ask', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'ap_page', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'qcat_id', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'qcat', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'qtag_id', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'q_tag', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'ap_s', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'parent', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'ap_user', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'user_page', $anspress_rewrite, true ) );
+		$this->assertTrue( in_array( 'ap_paged', $anspress_rewrite, true ) );
+	}
 }
