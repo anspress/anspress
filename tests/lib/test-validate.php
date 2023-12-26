1cb834926796fc9d50de2cd973fdbcbb7f6e1a0c
Bishal Napit <mebishalnapit@gmail.com>
2023-12-26 10:56:54 +0545

    Add - PHPUnit test case for the \AnsPress\Form\Validate::sanitize_text_field method

--- tests/lib/test-validate.php
+++ tests/lib/test-validate.php
@@ -43,4 +43,44 @@ class TestAnsPressFormValidate extends TestCase {
 		$this->assertTrue( method_exists( 'AnsPress\Form\Validate', 'validate_upload' ) );
 		$this->assertTrue( method_exists( 'AnsPress\Form\Validate', 'validate_is_checked' ) );
 	}
+
+	/**
+	 * @covers \AnsPress\Form\Validate::sanitize_text_field
+	 */
+	public function testValidateSanitizeTextField() {
+		// Normal test on strings.
+		$this->assertEquals( 'AnsPress Question Answer', \AnsPress\Form\Validate::sanitize_text_field( 'AnsPress   Question   Answer' ) );
+		$this->assertEquals( '\AnsPress', \AnsPress\Form\Validate::sanitize_text_field( '\\AnsPress     ' ) );
+		$this->assertEquals( '\AnsPress', \AnsPress\Form\Validate::sanitize_text_field( '     \\AnsPress     ' ) );
+		$this->assertEquals( 'Question title', \AnsPress\Form\Validate::sanitize_text_field( '<h1 class="entry-title">Question title</h1>' ) );
+		$this->assertEquals( 'Answer content', \AnsPress\Form\Validate::sanitize_text_field( '<p class="entry-content">Answer content</p>' ) );
+		$this->assertEquals( '&lt; AnsPress Question Answer &lt; Plugin', \AnsPress\Form\Validate::sanitize_text_field( '   <          AnsPress Question Answer < Plugin          ' ) );
+		$this->assertEquals( null, \AnsPress\Form\Validate::sanitize_text_field() );
+		$this->assertEquals( null, \AnsPress\Form\Validate::sanitize_text_field( '' ) );
+
+		// Test on arrays.
+		$arr = [
+			'AnsPress   Question   Answer',
+			'\\AnsPress     ',
+			'     \\AnsPress     ',
+			'<h1 class="entry-title">Question title</h1>',
+			'<p class="entry-content">Answer content</p>',
+			'   <          AnsPress Question Answer < Plugin          ',
+			'',
+			'',
+		];
+		$this->assertEquals(
+			[
+				'AnsPress Question Answer',
+				'\AnsPress',
+				'\AnsPress',
+				'Question title',
+				'Answer content',
+				'&lt; AnsPress Question Answer &lt; Plugin',
+				null,
+				null,
+			],
+			\AnsPress\Form\Validate::sanitize_text_field( $arr )
+		);
+	}
 }
