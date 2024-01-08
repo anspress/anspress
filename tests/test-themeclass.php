0e605bd57c9ebf38de01ab9eb1b13874ea563fc8
Bishal Napit <mebishalnapit@gmail.com>
2024-01-08 12:25:34 +0545

    Add - PHPUnit test case for the AnsPress_Theme::remove_hentry_class method

--- tests/test-themeclass.php
+++ tests/test-themeclass.php
@@ -6,6 +6,8 @@ use Yoast\WPTestUtils\WPIntegration\TestCase;
 
 class TestThemeClass extends TestCase {
 
+	use Testcases\Common;
+
 	public function testMethodExists() {
 		$this->assertTrue( method_exists( 'AnsPress_Theme', 'init_actions' ) );
 		$this->assertTrue( method_exists( 'AnsPress_Theme', 'template_include' ) );
@@ -30,5 +32,77 @@ class TestThemeClass extends TestCase {
 	public function testAnsPressThemeInitActions() {
 		$this->assertTrue( shortcode_exists( 'anspress' ) );
 		$this->assertTrue( shortcode_exists( 'question' ) );
+	}
+
+	/**
+	 * @covers AnsPress_Theme::remove_hentry_class
+	 */
+	public function testRemoveHentryClass() {
+		$id = $this->insert_answer();
+		$post_classes = [ 'hentry', 'other-classes' ];
+
+		// Test begins.
+		// For question post type.
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $id->q );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// For answer post type.
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $id->a );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// For other post types.
+		$post_id = $this->factory()->post->create();
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $post_id );
+		$this->assertContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+		$page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $page_id );
+		$this->assertContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// For AnsPress pages.
+		// Base page.
+		$base_page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		ap_opt( 'base_page', $base_page_id );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $base_page_id );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// Ask page.
+		$ask_page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		ap_opt( 'ask_page', $ask_page_id );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $ask_page_id );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// User page.
+		$user_page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		ap_opt( 'user_page', $user_page_id );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $user_page_id );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// Categories page.
+		$categories_page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		ap_opt( 'categories_page', $categories_page_id );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $categories_page_id );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// Tags page.
+		$tags_page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		ap_opt( 'tags_page', $tags_page_id );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $tags_page_id );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
+
+		// Activities page.
+		$activities_page_id = $this->factory()->post->create( [ 'post_type' => 'page' ] );
+		ap_opt( 'activities_page', $activities_page_id );
+		$result = \AnsPress_Theme::remove_hentry_class( $post_classes, '', $activities_page_id );
+		$this->assertNotContains( 'hentry', $result );
+		$this->assertContains( 'other-classes', $result );
 	}
 }
