9a7e17a482670807d1450e2c326bac24de645db8
Bishal Napit <mebishalnapit@gmail.com>
2024-01-10 12:50:09 +0545

    Add - PHPUnit test case for properties and method exists on Anspress\Addons\Reputation class

---
+++ tests/addons/test-reputation.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Anspress\Tests;
+
+use Yoast\WPTestUtils\WPIntegration\TestCase;
+
+class TestAddonReputation extends TestCase {
+
+	/**
+	 * @covers Anspress\Addons\Reputation::instance
+	 */
+	public function testInstance() {
+		$class = new \ReflectionClass( 'Anspress\Addons\Reputation' );
+		$this->assertTrue( $class->hasProperty( 'instance' ) && $class->getProperty( 'instance' )->isStatic() );
+	}
+
+	public function testMethodExists() {
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', '__construct' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'add_to_settings_page' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'load_options' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'register_default_events' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'ap_save_events' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'new_question' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'new_answer' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'trash_question' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'trash_answer' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'select_answer' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'unselect_answer' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'vote_up' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'vote_down' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'undo_vote_up' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'undo_vote_down' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'new_comment' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'delete_comment' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'user_register' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'delete_user' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'display_name' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'pre_fetch_post' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'bp_profile_header_meta' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'ap_user_pages' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'reputation_page' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'load_more_reputation' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'ap_bp_nav' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'ap_bp_page' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'bp_reputation_page' ) );
+		$this->assertTrue( method_exists( 'Anspress\Addons\Reputation', 'ap_all_options' ) );
+	}
+}
