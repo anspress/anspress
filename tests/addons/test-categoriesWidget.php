3048f818551bdfba2f603323896a0dadd5fd610e
Bishal Napit <mebishalnapit@gmail.com>
2024-01-10 10:25:01 +0545

    Add - PHPUnit test case for method exists on Anspress\Widgets\Categories class

---
+++ tests/addons/test-categoriesWidget.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Anspress\Tests;
+
+use Yoast\WPTestUtils\WPIntegration\TestCase;
+
+// Since this file is required on widget_init hook,
+// we include the file here directly for testing purpose.
+require_once ANSPRESS_ADDONS_DIR . '/categories/widget.php';
+
+class TestCategoriesWidget extends TestCase {
+
+	public function testMethodExists() {
+		$this->assertTrue( method_exists( 'Anspress\Widgets\Categories', '__construct' ) );
+		$this->assertTrue( method_exists( 'Anspress\Widgets\Categories', 'widget' ) );
+		$this->assertTrue( method_exists( 'Anspress\Widgets\Categories', 'form' ) );
+		$this->assertTrue( method_exists( 'Anspress\Widgets\Categories', 'update' ) );
+	}
+}
