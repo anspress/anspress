a7b1e04ef9af76e939a234a2bd37ee335f183a25
Bishal Napit <mebishalnapit@gmail.com>
2024-01-24 09:10:00 +0545

    Add - PHPUnit test case for the AP_Question_Meta_Box::__construct method

--- tests/admin/test-metaBox.php
+++ tests/admin/test-metaBox.php
@@ -13,4 +13,12 @@ class TestAPQuestionMetaBox extends TestCase {
 		$this->assertTrue( method_exists( 'AP_Question_Meta_Box', 'question_meta_box_content' ) );
 		$this->assertTrue( method_exists( 'AP_Question_Meta_Box', 'flag_meta_box' ) );
 	}
+
+	/**
+	 * @covers AP_Question_Meta_Box::__construct
+	 */
+	public function testConstruct() {
+		$meta_box = new \AP_Question_Meta_Box();
+		$this->assertEquals( 10, has_action( 'add_meta_boxes', [ $meta_box, 'add_meta_box' ] ) );
+	}
 }
