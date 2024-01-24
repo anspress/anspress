cf8a0bee532c6523c87868fb4653bf917090cce4
Bishal Napit <mebishalnapit@gmail.com>
2024-01-24 11:50:32 +0545

    Add - PHPUnit test case for the AP_License::__construct method

--- tests/admin/test-license.php
+++ tests/admin/test-license.php
@@ -13,4 +13,13 @@ class TestAPLicense extends TestCase {
 		$this->assertTrue( method_exists( 'AP_License', 'ap_product_license' ) );
 		$this->assertTrue( method_exists( 'AP_License', 'ap_plugin_updater' ) );
 	}
+
+	/**
+	 * @covers AP_License::__construct
+	 */
+	public function testConstruct() {
+		$license = new \AP_License();
+		$this->assertEquals( 10, has_action( 'ap_admin_menu', [ $license, 'menu' ] ) );
+		$this->assertEquals( 0, has_action( 'admin_init', [ $license, 'ap_plugin_updater' ] ) );
+	}
 }
