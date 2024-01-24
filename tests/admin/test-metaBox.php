9a30aa4db25f95775440a674ddcfcf1a0cc66d49
Bishal Napit <mebishalnapit@gmail.com>
2024-01-24 13:43:00 +0545

    Add - PHPUnit test case for the AP_Question_Meta_Box::flag_meta_box method

--- tests/admin/test-metaBox.php
+++ tests/admin/test-metaBox.php
@@ -67,4 +67,49 @@ class TestAPQuestionMetaBox extends TestCase {
 		$this->assertEquals( 'Question', $GLOBALS['wp_meta_boxes']['answer']['side']['high']['ap_question_meta_box']['title'] );
 		$this->assertEquals( [ $meta_box, 'question_meta_box_content' ], $GLOBALS['wp_meta_boxes']['answer']['side']['high']['ap_question_meta_box']['callback'] );
 	}
+
+	/**
+	 * @covers AP_Question_Meta_Box::flag_meta_box
+	 */
+	public function testFlagMetaBox() {
+		$meta_box = new \AP_Question_Meta_Box();
+		$id = $this->insert_answer();
+
+		// Store the result of the method in a variable.
+		ob_start();
+		$meta_box->flag_meta_box( get_post( $id->q ) );
+		$result = ob_get_clean();
+
+		// Test begins.
+		// Test 1.
+		$args = array(
+			'action'         => 'ap_ajax',
+			'ap_ajax_action' => 'ap_clear_flag',
+			'__nonce'        => wp_create_nonce( 'clear_flag_' . $id->q ),
+			'post_id'        => $id->q,
+		);
+		$this->assertStringContainsString( '<i class="apicon-flag"></i>', $result );
+		$this->assertStringContainsString( '<strong class="ap-question-flag-count">0</strong> ', $result );
+		$this->assertStringContainsString( 'Flag', $result );
+		$this->assertStringContainsString( '<a id="ap-clear-flag" href="#" data-query="' . esc_js( wp_json_encode( $args ) ) . '" class="flag-clear" data-cb="afterFlagClear">Clear flag</a>', $result );
+		$this->assertStringContainsString( '<script type="text/javascript">', $result );
+		$this->assertStringContainsString( '$(\'#ap-clear-flag\')', $result );
+		$this->assertStringContainsString( '$.ajax', $result );
+
+		// Test 2.
+		ap_add_flag( $id->q );
+		$user_id = $this->factory()->user->create();
+		ap_add_flag( $id->q, $user_id );
+		ap_update_flags_count( $id->q );
+		ob_start();
+		$meta_box->flag_meta_box( get_post( $id->q ) );
+		$result = ob_get_clean();
+		$this->assertStringContainsString( '<i class="apicon-flag"></i>', $result );
+		$this->assertStringContainsString( '<strong class="ap-question-flag-count">2</strong> ', $result );
+		$this->assertStringContainsString( 'Flag', $result );
+		$this->assertStringContainsString( '<a id="ap-clear-flag" href="#" data-query="' . esc_js( wp_json_encode( $args ) ) . '" class="flag-clear" data-cb="afterFlagClear">Clear flag</a>', $result );
+		$this->assertStringContainsString( '<script type="text/javascript">', $result );
+		$this->assertStringContainsString( '$(\'#ap-clear-flag\')', $result );
+		$this->assertStringContainsString( '$.ajax', $result );
+	}
 }
