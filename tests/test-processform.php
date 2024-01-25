2c976b0e03362e7ca01992cee7aaffe59f64c352
Bishal Napit <mebishalnapit@gmail.com>
2024-01-25 14:49:15 +0545

    Add - PHPUnit test case for the AnsPress_Process_Form::process_form method

--- tests/test-processform.php
+++ tests/test-processform.php
@@ -19,4 +19,29 @@ class TestProcessForm extends TestCase {
 		$this->assertTrue( method_exists( 'AnsPress_Process_Form', 'ap_ajax' ) );
 		$this->assertTrue( method_exists( 'AnsPress_Process_Form', 'process_form' ) );
 	}
+
+	/**
+	 * @covers AnsPress_Process_Form::process_form
+	 */
+	public function testProcessForm() {
+		// Test 1.
+		$_REQUEST['ap_form_action'] = 'some_action';
+		$hook_triggered = false;
+		add_action( 'ap_process_form_some_action', function() use ( &$hook_triggered ) {
+			$hook_triggered = true;
+		} );
+		$process_form = new \AnsPress_Process_Form();
+		$process_form->process_form();
+		$this->assertTrue( $hook_triggered );
+
+		// Test 2.
+		$_REQUEST['ap_form_action'] = 'another_action';
+		$hook_triggered = false;
+		add_action( 'ap_process_form_another_action', function() use ( &$hook_triggered ) {
+			$hook_triggered = true;
+		} );
+		$process_form = new \AnsPress_Process_Form();
+		$process_form->process_form();
+		$this->assertTrue( $hook_triggered );
+	}
 }
