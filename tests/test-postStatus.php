dc1eb3cc17f21b90b8b985c5a2a103017177947a
Bishal Napit <mebishalnapit@gmail.com>
2024-01-29 11:06:46 +0545

    Add - PHPUnit test case for the ap_get_post_status_message function

--- tests/test-postStatus.php
+++ tests/test-postStatus.php
@@ -22,4 +22,96 @@ class TestPostStatus extends TestCase {
 		$this->assertArrayHasKey( 'moderate', $wp_post_statuses );
 		$this->assertArrayHasKey( 'private_post', $wp_post_statuses );
 	}
+
+	/**
+	 * @covers ::ap_get_post_status_message
+	 */
+	public function testAPGetPostStatusMessage() {
+		// Test 1.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id ] );
+		$this->assertEquals( '', ap_get_post_status_message( $question_id ) );
+		$this->assertEquals( '', ap_get_post_status_message( $answer_id ) );
+
+		// Test 2.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'private_post' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'private_post' ] );
+		$this->assertStringContainsString( '<i class="apicon-lock"></i>', ap_get_post_status_message( $question_id ) );
+		$this->assertStringContainsString( 'This Question is marked as a private, only admin and post author can see.', ap_get_post_status_message( $question_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-private_post"><i class="apicon-lock"></i><span>This Question is marked as a private, only admin and post author can see.</span></div>',
+			ap_get_post_status_message( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-lock"></i>', ap_get_post_status_message( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer is marked as a private, only admin and post author can see.', ap_get_post_status_message( $answer_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-private_post"><i class="apicon-lock"></i><span>This Answer is marked as a private, only admin and post author can see.</span></div>',
+			ap_get_post_status_message( $answer_id )
+		);
+
+		// Test 3.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'moderate' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'moderate' ] );
+		$this->assertStringContainsString( '<i class="apicon-alert"></i>', ap_get_post_status_message( $question_id ) );
+		$this->assertStringContainsString( 'This Question is waiting for the approval by the moderator.', ap_get_post_status_message( $question_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-moderate"><i class="apicon-alert"></i><span>This Question is waiting for the approval by the moderator.</span></div>',
+			ap_get_post_status_message( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-alert"></i>', ap_get_post_status_message( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer is waiting for the approval by the moderator.', ap_get_post_status_message( $answer_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-moderate"><i class="apicon-alert"></i><span>This Answer is waiting for the approval by the moderator.</span></div>',
+			ap_get_post_status_message( $answer_id )
+		);
+
+		// Test 4.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question' ] );
+		ap_insert_qameta(
+			$question_id,
+			array(
+				'selected_id'  => '',
+				'last_updated' => current_time( 'mysql' ),
+				'closed'       => 1,
+			)
+		);
+		$this->assertStringContainsString( '<i class="apicon-x"></i>', ap_get_post_status_message( $question_id ) );
+		$this->assertStringContainsString( 'Question is closed for new answers.', ap_get_post_status_message( $question_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-publish closed"><i class="apicon-x"></i><span>Question is closed for new answers.</span></div>',
+			ap_get_post_status_message( $question_id )
+		);
+
+		// Test 5.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'trash' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'trash' ] );
+		$this->assertStringContainsString( '<i class="apicon-trashcan"></i>', ap_get_post_status_message( $question_id ) );
+		$this->assertStringContainsString( 'This Question has been trashed, you can delete it permanently from wp-admin.', ap_get_post_status_message( $question_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-trash"><i class="apicon-trashcan"></i><span>This Question has been trashed, you can delete it permanently from wp-admin.</span></div>',
+			ap_get_post_status_message( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-trashcan"></i>', ap_get_post_status_message( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer has been trashed, you can delete it permanently from wp-admin.', ap_get_post_status_message( $answer_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-trash"><i class="apicon-trashcan"></i><span>This Answer has been trashed, you can delete it permanently from wp-admin.</span></div>',
+			ap_get_post_status_message( $answer_id )
+		);
+
+		// Test 6.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'future', 'post_date' => '9999-12-31 23:59:59', ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'future', 'post_date' => '9999-12-31 23:59:59', ] );
+		$this->assertStringContainsString( '<i class="apicon-clock"></i>', ap_get_post_status_message( $question_id ) );
+		$this->assertStringContainsString( 'This Question is not published yet and is not accessible to anyone until it get published.', ap_get_post_status_message( $question_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-future"><i class="apicon-clock"></i><span>This Question is not published yet and is not accessible to anyone until it get published.</span></div>',
+			ap_get_post_status_message( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-clock"></i>', ap_get_post_status_message( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer is not published yet and is not accessible to anyone until it get published.', ap_get_post_status_message( $answer_id ) );
+		$this->assertEquals(
+			'<div class="ap-notice status-future"><i class="apicon-clock"></i><span>This Answer is not published yet and is not accessible to anyone until it get published.</span></div>',
+			ap_get_post_status_message( $answer_id )
+		);
+	}
 }
