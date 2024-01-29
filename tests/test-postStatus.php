387e40526c630cd7982a6c65651296b02d69ec9c
Bishal Napit <mebishalnapit@gmail.com>
2024-01-29 11:27:09 +0545

    Add - PHPUnit test case for the ap_post_status_badge function

--- tests/test-postStatus.php
+++ tests/test-postStatus.php
@@ -114,4 +114,96 @@ class TestPostStatus extends TestCase {
 			ap_get_post_status_message( $answer_id )
 		);
 	}
+
+	/**
+	 * @covers ::ap_post_status_badge
+	 */
+	public function testAPPostStatusBadge() {
+		// Test 1.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id ] );
+		$this->assertEquals( '<postmessage></postmessage>', ap_post_status_badge( $question_id ) );
+		$this->assertEquals( '<postmessage></postmessage>', ap_post_status_badge( $answer_id ) );
+
+		// Test 2.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'private_post' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'private_post' ] );
+		$this->assertStringContainsString( '<i class="apicon-lock"></i>', ap_post_status_badge( $question_id ) );
+		$this->assertStringContainsString( 'This Question is marked as a private, only admin and post author can see.', ap_post_status_badge( $question_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-private_post"><i class="apicon-lock"></i><span>This Question is marked as a private, only admin and post author can see.</span></div></postmessage>',
+			ap_post_status_badge( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-lock"></i>', ap_post_status_badge( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer is marked as a private, only admin and post author can see.', ap_post_status_badge( $answer_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-private_post"><i class="apicon-lock"></i><span>This Answer is marked as a private, only admin and post author can see.</span></div></postmessage>',
+			ap_post_status_badge( $answer_id )
+		);
+
+		// Test 3.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'moderate' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'moderate' ] );
+		$this->assertStringContainsString( '<i class="apicon-alert"></i>', ap_post_status_badge( $question_id ) );
+		$this->assertStringContainsString( 'This Question is waiting for the approval by the moderator.', ap_post_status_badge( $question_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-moderate"><i class="apicon-alert"></i><span>This Question is waiting for the approval by the moderator.</span></div></postmessage>',
+			ap_post_status_badge( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-alert"></i>', ap_post_status_badge( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer is waiting for the approval by the moderator.', ap_post_status_badge( $answer_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-moderate"><i class="apicon-alert"></i><span>This Answer is waiting for the approval by the moderator.</span></div></postmessage>',
+			ap_post_status_badge( $answer_id )
+		);
+
+		// Test 4.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question' ] );
+		ap_insert_qameta(
+			$question_id,
+			array(
+				'selected_id'  => '',
+				'last_updated' => current_time( 'mysql' ),
+				'closed'       => 1,
+			)
+		);
+		$this->assertStringContainsString( '<i class="apicon-x"></i>', ap_post_status_badge( $question_id ) );
+		$this->assertStringContainsString( 'Question is closed for new answers.', ap_post_status_badge( $question_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-publish closed"><i class="apicon-x"></i><span>Question is closed for new answers.</span></div></postmessage>',
+			ap_post_status_badge( $question_id )
+		);
+
+		// Test 5.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'trash' ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'trash' ] );
+		$this->assertStringContainsString( '<i class="apicon-trashcan"></i>', ap_post_status_badge( $question_id ) );
+		$this->assertStringContainsString( 'This Question has been trashed, you can delete it permanently from wp-admin.', ap_post_status_badge( $question_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-trash"><i class="apicon-trashcan"></i><span>This Question has been trashed, you can delete it permanently from wp-admin.</span></div></postmessage>',
+			ap_post_status_badge( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-trashcan"></i>', ap_post_status_badge( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer has been trashed, you can delete it permanently from wp-admin.', ap_post_status_badge( $answer_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-trash"><i class="apicon-trashcan"></i><span>This Answer has been trashed, you can delete it permanently from wp-admin.</span></div></postmessage>',
+			ap_post_status_badge( $answer_id )
+		);
+
+		// Test 6.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'future', 'post_date' => '9999-12-31 23:59:59', ] );
+		$answer_id   = $this->factory->post->create( [ 'post_type' => 'answer', 'post_parent' => $question_id, 'post_status' => 'future', 'post_date' => '9999-12-31 23:59:59', ] );
+		$this->assertStringContainsString( '<i class="apicon-clock"></i>', ap_post_status_badge( $question_id ) );
+		$this->assertStringContainsString( 'This Question is not published yet and is not accessible to anyone until it get published.', ap_post_status_badge( $question_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-future"><i class="apicon-clock"></i><span>This Question is not published yet and is not accessible to anyone until it get published.</span></div></postmessage>',
+			ap_post_status_badge( $question_id )
+		);
+		$this->assertStringContainsString( '<i class="apicon-clock"></i>', ap_post_status_badge( $answer_id ) );
+		$this->assertStringContainsString( 'This Answer is not published yet and is not accessible to anyone until it get published.', ap_post_status_badge( $answer_id ) );
+		$this->assertEquals(
+			'<postmessage><div class="ap-notice status-future"><i class="apicon-clock"></i><span>This Answer is not published yet and is not accessible to anyone until it get published.</span></div></postmessage>',
+			ap_post_status_badge( $answer_id )
+		);
+	}
 }
