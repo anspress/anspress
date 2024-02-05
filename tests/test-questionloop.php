2d82060a126c751c028f3d7b51a0fa4932dd0add
Bishal Napit <mebishalnapit@gmail.com>
2024-02-05 12:10:08 +0545

    Add - PHPUnit test case for the ap_get_questions function

--- tests/test-questionloop.php
+++ tests/test-questionloop.php
@@ -238,4 +238,132 @@ class TestQustionLoop extends TestCase {
 		$this->assertEquals( $question_id, $question->post->ID );
 		$this->logout();
 	}
+
+	/**
+	 * @covers ::ap_get_questions
+	 */
+	public function testAPGetQuestions() {
+		// Test on other pages.
+		// Test 1.
+		$questions = ap_get_questions();
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( ap_opt( 'question_per_page' ), $questions->query_vars['showposts'] );
+		$this->assertEquals( 1, $questions->query_vars['paged'] );
+		$this->assertEquals( 'featured_post', $questions->query_vars['ap_query'] );
+		$this->assertEmpty( $questions->query_vars['post_parent'] );
+
+		// Test 2.
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'publish' ] );
+		$this->go_to( '?post_type=question&p=' . $question_id );
+		set_query_var( 'paged', 3 );
+		$questions = ap_get_questions();
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( ap_opt( 'question_per_page' ), $questions->query_vars['showposts'] );
+		$this->assertEquals( 3, $questions->query_vars['paged'] );
+		$this->assertEquals( 'featured_post', $questions->query_vars['ap_query'] );
+		$this->assertEmpty( $questions->query_vars['post_parent'] );
+
+		// Test 3.
+		$args = [
+			'showposts'   => 11,
+			'paged'       => 3,
+			'ap_query'    => 'single_question',
+			'post_parent' => 11,
+			'ap_order_by' => 'newest',
+		];
+		$questions = ap_get_questions( $args );
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( 11, $questions->query_vars['showposts'] );
+		$this->assertEquals( 3, $questions->query_vars['paged'] );
+		$this->assertEquals( 'single_question', $questions->query_vars['ap_query'] );
+		$this->assertEquals( 11, $questions->query_vars['post_parent'] );
+		$this->assertEquals( 'newest', $questions->query_vars['ap_order_by'] );
+
+		// Test 4.
+		$this->go_to( '?post_type=question&p=' . $question_id );
+		$answer_id = $this->factory->post->create( [ 'post_type' => 'answer', 'post_status' => 'publish', 'post_parent' => $question_id ] );
+		ap_opt( 'question_per_page', 3 );
+		set_query_var( 'paged', 5 );
+		$args = [
+			'ap_query'    => 'single_question',
+			'post_parent' => $question_id,
+			'ap_order_by' => 'oldest',
+			'p'           => $answer_id,
+		];
+		$questions = ap_get_questions( $args );
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( 3, $questions->query_vars['showposts'] );
+		$this->assertEquals( 5, $questions->query_vars['paged'] );
+		$this->assertEquals( 'single_question', $questions->query_vars['ap_query'] );
+		$this->assertEquals( $question_id, $questions->query_vars['post_parent'] );
+		$this->assertEquals( 'oldest', $questions->query_vars['ap_order_by'] );
+		$this->assertEquals( $answer_id, $questions->query_vars['p'] );
+
+		// Test for front page.
+		$page_id = $this->factory->post->create( [ 'post_type' => 'page' ] );
+		update_option( 'page_on_front', $page_id );
+		update_option( 'show_on_front', 'page' );
+		$question_id = $this->factory->post->create( [ 'post_type' => 'question', 'post_status' => 'publish' ] );
+
+		// Test 1.
+		$this->go_to( home_url() );
+		$questions = ap_get_questions();
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( ap_opt( 'question_per_page' ), $questions->query_vars['showposts'] );
+		$this->assertEquals( 1, $questions->query_vars['paged'] );
+		$this->assertEquals( 'featured_post', $questions->query_vars['ap_query'] );
+		$this->assertEmpty( $questions->query_vars['post_parent'] );
+
+		// Test 2.
+		$this->go_to( home_url() );
+		ap_opt( 'question_per_page', 5 );
+		$_REQUEST['ap_paged'] = 3;
+		$questions = ap_get_questions();
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( 5, $questions->query_vars['showposts'] );
+		$this->assertEquals( 3, $questions->query_vars['paged'] );
+		$this->assertEquals( 'featured_post', $questions->query_vars['ap_query'] );
+		$this->assertEmpty( $questions->query_vars['post_parent'] );
+
+		// Test 3.
+		$this->go_to( home_url() );
+		unset( $_REQUEST['ap_paged'] );
+		ap_opt( 'question_per_page', 5 );
+		$args = [
+			'ap_query'    => 'single_question',
+			'post_parent' => $question_id,
+			'ap_order_by' => 'newest',
+		];
+		$questions = ap_get_questions( $args );
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( 5, $questions->query_vars['showposts'] );
+		$this->assertEquals( 1, $questions->query_vars['paged'] );
+		$this->assertEquals( 'single_question', $questions->query_vars['ap_query'] );
+		$this->assertEquals( $question_id, $questions->query_vars['post_parent'] );
+		$this->assertEquals( 'newest', $questions->query_vars['ap_order_by'] );
+
+		// Test 4.
+		$this->go_to( home_url() );
+		$answer_id = $this->factory->post->create( [ 'post_type' => 'answer', 'post_status' => 'publish', 'post_parent' => $question_id ] );
+		$_REQUEST['ap_paged'] = 3;
+		$args = [
+			'ap_query'    => 'single_question',
+			'post_parent' => $question_id,
+			'p'           => $answer_id,
+		];
+		$questions = ap_get_questions( $args );
+		$this->assertInstanceOf( 'Question_Query', $questions );
+		$this->assertEquals( ap_opt( 'question_per_page' ), $questions->query_vars['showposts'] );
+		$this->assertEquals( 3, $questions->query_vars['paged'] );
+		$this->assertEquals( 'single_question', $questions->query_vars['ap_query'] );
+		$this->assertEquals( $question_id, $questions->query_vars['post_parent'] );
+		$this->assertEquals( $answer_id, $questions->query_vars['p'] );
+
+		// Reset the front page.
+		update_option( 'page_on_front', 0 );
+		update_option( 'show_on_front', 'posts' );
+
+		// Reset the question_per_page option.
+		ap_opt( 'question_per_page', 20 );
+	}
 }
