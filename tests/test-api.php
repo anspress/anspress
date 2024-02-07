15eef77d63c9e4b6695d032d224877f436eb2499
Bishal Napit <mebishalnapit@gmail.com>
2024-02-07 13:50:10 +0545

    Add - PHPUnit test case for the AnsPress_API::avatar method

--- tests/test-api.php
+++ tests/test-api.php
@@ -13,4 +13,26 @@ class TestAPI extends TestCase {
 		$this->assertTrue( method_exists( 'AnsPress_API', 'register' ) );
 		$this->assertTrue( method_exists( 'AnsPress_API', 'avatar' ) );
 	}
+
+	/**
+	 * @covers AnsPress_API::avatar
+	 */
+	public function testAvatar() {
+		// Test 1.
+		$request = new \WP_REST_Request();
+		$user_id = $this->factory->user->create();
+		$request->set_query_params( array(
+			'id'   => $user_id,
+			'size' => 50,
+		) );
+		$response = \AnsPress_API::avatar( $request );
+		$this->assertInstanceOf( 'WP_REST_Response', $response );
+		$this->assertEquals( 200, $response->get_status() );
+
+		// Test 2.
+		$request->set_query_params( array() );
+		$response = \AnsPress_API::avatar( $request );
+		$this->assertInstanceOf( 'WP_Error', $response );
+		$this->assertEquals( 'wrongData', $response->get_error_code() );
+	}
 }
