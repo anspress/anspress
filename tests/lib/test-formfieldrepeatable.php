0b313952dbb3989d5cf4689a281530060e9166d1
Bishal Napit <mebishalnapit@gmail.com>
2024-02-12 15:11:17 +0545

    Add - PHPUnit test case for the AnsPress\Form\Field\Repeatable::html_order method

--- tests/lib/test-formfieldrepeatable.php
+++ tests/lib/test-formfieldrepeatable.php
@@ -21,4 +21,36 @@ class TestAnsPressFormFieldRepeatable extends TestCase {
 		$this->assertTrue( method_exists( 'AnsPress\Form\Field\Repeatable', 'get_groups_count' ) );
 		$this->assertTrue( method_exists( 'AnsPress\Form\Field\Repeatable', 'field_markup' ) );
 	}
+
+	/**
+	 * @covers AnsPress\Form\Field\Repeatable::html_order
+	 */
+	public function testHTMLOrder() {
+		$field = new \AnsPress\Form\Field\Repeatable( 'Sample Form', 'sample-form', [ 'fields' => [] ] );
+		$reflection = new \ReflectionClass( $field );
+		$method = $reflection->getMethod( 'html_order' );
+		$method->setAccessible( true );
+		$property = $reflection->getProperty( 'output_order' );
+		$property->setAccessible( true );
+
+		// Test begins.
+		$accepted_html_order = [ 'wrapper_start', 'label', 'field_wrap_start', 'desc', 'errors', 'field_markup', 'field_wrap_end', 'wrapper_end' ];
+
+		// Before method is called.
+		$this->assertEmpty( $property->getValue( $field ) );
+
+		// After method is called.
+		// Test 1.
+		$method->invoke( $field );
+		$this->assertNotEmpty( $property->getValue( $field ) );
+		$this->assertEquals( $accepted_html_order, $property->getValue( $field ) );
+
+		// Test 2.
+		$custom_output_order = [ 'label', 'desc', 'errors', 'field_markup' ];
+		$field->args['output_order'] = $custom_output_order;
+		$method->invoke( $field );
+		$this->assertNotEmpty( $property->getValue( $field ) );
+		$this->assertEquals( $accepted_html_order, $property->getValue( $field ) );
+		$this->assertNotEquals( $custom_output_order, $property->getValue( $field ) );
+	}
 }
