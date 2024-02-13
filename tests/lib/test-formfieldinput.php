e4e1a2b9f45ce3d8924f42635f19b57e1e6e5c91
Bishal Napit <mebishalnapit@gmail.com>
2024-02-13 08:56:09 +0545

    Add - PHPUnit test case for the AnsPress\Form\Field\Input::set_subtype method

--- tests/lib/test-formfieldinput.php
+++ tests/lib/test-formfieldinput.php
@@ -18,4 +18,42 @@ class TestAnsPressFormFieldInput extends TestCase {
 		$this->assertTrue( method_exists( 'AnsPress\Form\Field\Input', 'html_order' ) );
 		$this->assertTrue( method_exists( 'AnsPress\Form\Field\Input', 'field_markup' ) );
 	}
+
+	/**
+	 * @covers AnsPress\Form\Field\Input::set_subtype
+	 */
+	public function testSetSubtype() {
+		$field = new \AnsPress\Form\Field\Input( 'Sample Form', 'sample-form', [] );
+		$reflection = new \ReflectionClass( $field );
+		$method = $reflection->getMethod( 'set_subtype' );
+		$method->setAccessible( true );
+
+		// Test begins.
+		// Before method is called.
+		$this->assertEquals( 'text', $field->subtype );
+
+		// After method is called.
+		// Test 1.
+		$field->args['subtype'] = 'text';
+		$method->invoke( $field );
+		$this->assertEquals( 'text', $field->subtype );
+
+		// Test 2.
+		$field->args['subtype'] = 'number';
+		$method->invoke( $field );
+		$this->assertEquals( 'number', $field->subtype );
+
+		// Test 3.
+		$field->args['subtype'] = 'color';
+		$method->invoke( $field );
+		$this->assertEquals( 'color', $field->subtype );
+
+		// Test 4.
+		$field->args['subtype'] = 'invalid';
+		$method->invoke( $field );
+		$this->assertEquals( 'text', $field->subtype );
+
+		// Reset subtype.
+		$field->args['subtype'] = 'text';
+	}
 }
