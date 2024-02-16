9735c581ec23893a0bf30567702602a4f064d0d7
Bishal Napit <mebishalnapit@gmail.com>
2024-02-16 09:03:06 +0545

    Tweak - Use ap_installed instead of new anspress_installed option for not re-activating the default addons on plugin re-activation

--- activate.php
+++ activate.php
@@ -125,8 +125,8 @@ class AP_Activate {
 	 * @since 4.1.8 Fixed #425
 	 */
 	public function enable_addons() {
-		// Return if `anspress_installed` plugin option is available.
-		if ( ap_opt( 'anspress_installed' ) ) {
+		// Return if `ap_installed` option is available.
+		if ( ap_opt( 'ap_installed' ) ) {
 			return;
 		}
 
@@ -134,9 +134,6 @@ class AP_Activate {
 		ap_activate_addon( 'reputation.php' );
 		ap_activate_addon( 'email.php' );
 		ap_activate_addon( 'categories.php' );
-
-		// Update plugin option with `anspress_installed` for setting a flag.
-		ap_opt( 'anspress_installed', 'true' );
 	}
 
 	/**
