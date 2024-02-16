6db6d170d10d67563badcfe0a8a79a3cfc1d3cd5
Bishal Napit <mebishalnapit@gmail.com>
2024-02-16 13:51:20 +0545

    Add - PHPUnit test case for the ap_notification_verbs function

--- tests/addons/test-notificationsFunctions.php
+++ tests/addons/test-notificationsFunctions.php
@@ -142,4 +142,79 @@ class TestAddonNotificationsFunctions extends TestCase {
 		// Reset global variable.
 		$ap_notification_verbs = [];
 	}
+
+	/**
+	 * @covers ::ap_notification_verbs
+	 */
+	public function testAPNotificationVerbs() {
+		global $ap_notification_verbs;
+		// Set up the action hook callback
+		$callback_triggered = false;
+		add_action( 'ap_notification_verbs', function() use ( &$callback_triggered ) {
+			$callback_triggered = true;
+		} );
+
+		// Test begins.
+		// Test 1.
+		$this->assertFalse( $callback_triggered );
+		ap_notification_verbs();
+		$this->assertIsArray( $ap_notification_verbs );
+		$this->assertEmpty( $ap_notification_verbs );
+		$this->assertTrue( $callback_triggered );
+
+		// Test 2.
+		$callback_triggered = false;
+		$ap_notification_verbs = [ 'test-verb' => [ 'label' => 'Test Verb' ] ];
+		$this->assertFalse( $callback_triggered );
+		ap_notification_verbs();
+		$this->assertIsArray( $ap_notification_verbs );
+		$this->assertArrayHasKey( 'test-verb', $ap_notification_verbs );
+		$expected = [ 'label' => 'Test Verb' ];
+		$this->assertEquals( $expected, $ap_notification_verbs[ 'test-verb' ] );
+		$this->assertFalse( $callback_triggered );
+
+		// Test 3.
+		$callback_triggered = false;
+		$ap_notification_verbs = [];
+		$this->assertFalse( $callback_triggered );
+		ap_notification_verbs();
+		$this->assertIsArray( $ap_notification_verbs );
+		$this->assertEmpty( $ap_notification_verbs );
+		$this->assertTrue( $callback_triggered );
+
+		// Test 4.
+		$callback_triggered = false;
+		$ap_notification_verbs = [ 'test-verb-2' => [ 'label' => 'Test Verb 2' ] ];
+		$this->assertFalse( $callback_triggered );
+		ap_notification_verbs();
+		$this->assertIsArray( $ap_notification_verbs );
+		$this->assertArrayHasKey( 'test-verb-2', $ap_notification_verbs );
+		$expected = [ 'label' => 'Test Verb 2' ];
+		$this->assertEquals( $expected, $ap_notification_verbs[ 'test-verb-2' ] );
+		$this->assertFalse( $callback_triggered );
+
+		// Test 5.
+		$callback_triggered = false;
+		$ap_notification_verbs = [
+			'test-verb-3' => [ 'label' => 'Test Verb 3' ],
+			'test-verb-4' => [ 'label' => 'Test Verb 4' ],
+			'test-verb-5' => [ 'label' => 'Test Verb 5' ],
+		];
+		$this->assertFalse( $callback_triggered );
+		ap_notification_verbs();
+		$this->assertIsArray( $ap_notification_verbs );
+		$this->assertArrayHasKey( 'test-verb-3', $ap_notification_verbs );
+		$expected = [ 'label' => 'Test Verb 3' ];
+		$this->assertEquals( $expected, $ap_notification_verbs[ 'test-verb-3' ] );
+		$this->assertArrayHasKey( 'test-verb-4', $ap_notification_verbs );
+		$expected = [ 'label' => 'Test Verb 4' ];
+		$this->assertEquals( $expected, $ap_notification_verbs[ 'test-verb-4' ] );
+		$this->assertArrayHasKey( 'test-verb-5', $ap_notification_verbs );
+		$expected = [ 'label' => 'Test Verb 5' ];
+		$this->assertEquals( $expected, $ap_notification_verbs[ 'test-verb-5' ] );
+		$this->assertFalse( $callback_triggered );
+
+		// Reset global variable.
+		$ap_notification_verbs = [];
+	}
 }
