6ddc2c44e6d0321963a70c59cb73f8f17851640a
Bishal Napit <mebishalnapit@gmail.com>
2024-02-20 15:15:33 +0545

    Add - PHPUnit test case for the AnsPress\Form\Field\Input::html_order method

--- tests/lib/test-formfieldinput.php
+++ tests/lib/test-formfieldinput.php
@@ -56,4 +56,83 @@ class TestAnsPressFormFieldInput extends TestCase {
 		// Reset subtype.
 		$field->args['subtype'] = 'text';
 	}
+
+	/**
+	 * @covers AnsPress\Form\Field\Input::html_order
+	 */
+	public function testHTMLOrder() {
+		$field = new \AnsPress\Form\Field\Input( 'Sample Form', 'sample-form', [] );
+		$reflection = new \ReflectionClass( $field );
+		$method = $reflection->getMethod( 'html_order' );
+		$method->setAccessible( true );
+		$property = $reflection->getProperty( 'output_order' );
+		$property->setAccessible( true );
+
+		// Test begins.
+		// Test 1.
+		$field->subtype = 'hidden';
+		$hidden_output_order = [ 'wrapper_start', 'errors', 'field_markup', 'wrapper_end' ];
+		$method->invoke( $field );
+		$this->assertEquals( $hidden_output_order, $property->getValue( $field ) );
+
+		// Test 2.
+		$field->subtype = 'text';
+		$default_output_order = [ 'wrapper_start', 'label', 'field_wrap_start', 'errors', 'field_markup', 'desc', 'field_wrap_end', 'wrapper_end' ];
+		$method->invoke( $field );
+		$this->assertEquals( $default_output_order, $property->getValue( $field ) );
+
+		// Test 3.
+		$custom_output_order = [ 'wrapper_start', 'label', 'desc', 'errors', 'field_wrap_start', 'field_markup', 'field_wrap_end', 'wrapper_end' ];
+		$field->args['output_order'] = $custom_output_order;
+		$method->invoke( $field );
+		$this->assertEquals( $custom_output_order, $property->getValue( $field ) );
+
+		// Test 4.
+		$new_custom_output_order = [ 'label', 'desc', 'errors', 'field_markup' ];
+		$field->args['output_order'] = $new_custom_output_order;
+		$method->invoke( $field );
+		$this->assertEquals( $new_custom_output_order, $property->getValue( $field ) );
+
+		// Test 5.
+		$field->args['output_order'] = [];
+		$method->invoke( $field );
+		$this->assertEquals( $default_output_order, $property->getValue( $field ) );
+
+		// Test 6.
+		$field->subtype = 'hidden';
+		$field->args['output_order'] = [];
+		$method->invoke( $field );
+		$this->assertEquals( $hidden_output_order, $property->getValue( $field ) );
+
+		// Test 7.
+		$field->subtype = 'text';
+		$field->args['output_order'] = $default_output_order;
+		$field->args['html'] = '<div>Custom HTML</div>';
+		$field->args['label'] = '';
+		$html_output_order = [ 0 => 'wrapper_start', 3 => 'errors', 4 => 'field_markup', 5 => 'desc', 7 => 'wrapper_end' ];
+		$method->invoke( $field );
+		$this->assertEquals( $html_output_order, $property->getValue( $field ) );
+
+		// Test 8.
+		$field->args['output_order'] = $new_custom_output_order;
+		$field->args['html'] = '<div>Custom HTML</div>';
+		$field->args['label'] = '';
+		$new_html_output_order = [ 1 => 'desc', 2 => 'errors', 3 => 'field_markup' ];
+		$method->invoke( $field );
+		$this->assertEquals( $new_html_output_order, $property->getValue( $field ) );
+
+		// Test 9.
+		$field->args['output_order'] = $default_output_order;
+		$field->args['html'] = '<div>Custom HTML</div>';
+		$field->args['label'] = 'Label';
+		$method->invoke( $field );
+		$this->assertEquals( $default_output_order, $property->getValue( $field ) );
+
+		// Test 10.
+		$field->args['output_order'] = $default_output_order;
+		$field->args['html'] = '';
+		$field->args['label'] = 'Label';
+		$method->invoke( $field );
+		$this->assertEquals( $default_output_order, $property->getValue( $field ) );
+	}
 }
