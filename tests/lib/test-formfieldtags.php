cba0cdb84c45521cca2fe4e1612371c1fe633f4b
Bishal Napit <mebishalnapit@gmail.com>
2024-02-22 13:51:17 +0545

    Add - PHPUnit test case for the AnsPress\Form\Field\Tags::prepare method

--- tests/lib/test-formfieldtags.php
+++ tests/lib/test-formfieldtags.php
@@ -20,6 +20,170 @@ class TestAnsPressFormFieldTags extends TestCase {
 	}
 
 	/**
+	 * @covers \AnsPress\Form\Field\Tags::prepare
+	 */
+	public function testPrepare() {
+		// Test 1.
+		$field = new \AnsPress\Form\Field\Tags( 'Sample Form', 'sample-form', [] );
+		$reflection = new \ReflectionClass( $field );
+		$sanitize_cb = $reflection->getProperty( 'sanitize_cb' );
+		$sanitize_cb->setAccessible( true );
+		$valiate_cb = $reflection->getProperty( 'validate_cb' );
+		$valiate_cb->setAccessible( true );
+		// No need to invoke prepare() method as it is called in constructor.
+		$this->assertIsArray( $sanitize_cb->getValue( $field ) );
+		$this->assertIsArray( $valiate_cb->getValue( $field ) );
+		$this->assertEquals( [ 'array_remove_empty', 'tags_field' ], $sanitize_cb->getValue( $field ) );
+		$this->assertEquals( [ 'is_array', 'array_max', 'array_min' ], $valiate_cb->getValue( $field ) );
+		$expected = [
+			'label'      => 'AnsPress Tags Field',
+			'array_max'  => 3,
+			'array_min'  => 2,
+			'terms_args' => [ 'taxonomy' => 'question_tag', 'hide_empty' => false, 'fields' => 'id=>name' ],
+			'options'    => 'terms',
+			'js_options' => [
+				'maxItems' => 3,
+				'form'     => 'Sample Form',
+				'id'       => 'SampleForm-sample-form',
+				'field'    => 'sample-form',
+				'nonce'    => wp_create_nonce( 'tags_Sample Formsample-form' ),
+				'create'   => false,
+				'labelAdd' => 'Add',
+			],
+		];
+		$this->assertEquals( $expected, $field->args );
+
+		// Test 2.
+		$field = new \AnsPress\Form\Field\Tags( 'Sample Form', 'sample-form', [
+			'label'     => 'Test Label',
+			'array_max' => 5,
+			'array_min' => 2,
+		] );
+		$reflection = new \ReflectionClass( $field );
+		$sanitize_cb = $reflection->getProperty( 'sanitize_cb' );
+		$sanitize_cb->setAccessible( true );
+		$valiate_cb = $reflection->getProperty( 'validate_cb' );
+		$valiate_cb->setAccessible( true );
+		// No need to invoke prepare() method as it is called in constructor.
+		$this->assertIsArray( $sanitize_cb->getValue( $field ) );
+		$this->assertIsArray( $valiate_cb->getValue( $field ) );
+		$this->assertEquals( [ 'array_remove_empty', 'tags_field' ], $sanitize_cb->getValue( $field ) );
+		$this->assertEquals( [ 'is_array', 'array_max', 'array_min' ], $valiate_cb->getValue( $field ) );
+		$expected = [
+			'label'      => 'Test Label',
+			'array_max'  => 5,
+			'array_min'  => 2,
+			'terms_args' => [ 'taxonomy' => 'question_tag', 'hide_empty' => false, 'fields' => 'id=>name' ],
+			'options'    => 'terms',
+			'js_options' => [
+				'maxItems' => 5,
+				'form'     => 'Sample Form',
+				'id'       => 'SampleForm-sample-form',
+				'field'    => 'sample-form',
+				'nonce'    => wp_create_nonce( 'tags_Sample Formsample-form' ),
+				'create'   => false,
+				'labelAdd' => 'Add',
+			],
+		];
+		$this->assertEquals( $expected, $field->args );
+
+		// Test 3.
+		$field = new \AnsPress\Form\Field\Tags( 'Sample Form', 'sample-form', [
+			'array_max'  => 10,
+			'array_min'  => 3,
+			'options'    => 'posts',
+			'js_options' => [],
+			'sanitize'   => 'custom_sanitize_cb',
+			'validate'   => 'custom_validate_cb',
+		] );
+		$reflection = new \ReflectionClass( $field );
+		$sanitize_cb = $reflection->getProperty( 'sanitize_cb' );
+		$sanitize_cb->setAccessible( true );
+		$valiate_cb = $reflection->getProperty( 'validate_cb' );
+		$valiate_cb->setAccessible( true );
+		// No need to invoke prepare() method as it is called in constructor.
+		$this->assertIsArray( $sanitize_cb->getValue( $field ) );
+		$this->assertIsArray( $valiate_cb->getValue( $field ) );
+		$this->assertEquals( [ 'array_remove_empty', 'tags_field', 'custom_sanitize_cb' ], $sanitize_cb->getValue( $field ) );
+		$this->assertEquals( [ 'is_array', 'array_max', 'array_min', 'custom_validate_cb' ], $valiate_cb->getValue( $field ) );
+		$expected = [
+			'label'      => 'AnsPress Tags Field',
+			'array_max'  => 10,
+			'array_min'  => 3,
+			'terms_args' => [ 'taxonomy' => 'question_tag', 'hide_empty' => false, 'fields' => 'id=>name' ],
+			'options'    => 'posts',
+			'js_options' => [
+				'maxItems' => 10,
+				'form'     => 'Sample Form',
+				'id'       => 'SampleForm-sample-form',
+				'field'    => 'sample-form',
+				'nonce'    => wp_create_nonce( 'tags_Sample Formsample-form' ),
+				'create'   => false,
+				'labelAdd' => 'Add',
+			],
+			'sanitize'   => 'custom_sanitize_cb',
+			'validate'   => 'custom_validate_cb',
+		];
+		$this->assertEquals( $expected, $field->args );
+
+		// Test 4.
+		$field = new \AnsPress\Form\Field\Tags( 'Sample Form', 'sample-form', [
+			'label'      => 'Test Label',
+			'array_max'  => 10,
+			'array_min'  => 3,
+			'options'    => [
+				'option1' => 'Option 1',
+				'option2' => 'Option 2',
+				'option3' => 'Option 3',
+			],
+			'js_options' => [
+				'maxItems' => 10,
+				'form'     => 'Test Form',
+				'id'       => 'test-form',
+				'field'    => 'test-form',
+				'nonce'    => wp_create_nonce( 'tags_Test Formtest-form' ),
+				'create'   => false,
+				'labelAdd' => 'New Tag',
+			],
+			'sanitize'   => 'custom_sanitize_cb',
+			'validate'   => 'custom_validate_cb',
+		] );
+		$reflection = new \ReflectionClass( $field );
+		$sanitize_cb = $reflection->getProperty( 'sanitize_cb' );
+		$sanitize_cb->setAccessible( true );
+		$valiate_cb = $reflection->getProperty( 'validate_cb' );
+		$valiate_cb->setAccessible( true );
+		// No need to invoke prepare() method as it is called in constructor.
+		$this->assertIsArray( $sanitize_cb->getValue( $field ) );
+		$this->assertIsArray( $valiate_cb->getValue( $field ) );
+		$this->assertEquals( [ 'array_remove_empty', 'tags_field', 'custom_sanitize_cb' ], $sanitize_cb->getValue( $field ) );
+		$this->assertEquals( [ 'is_array', 'array_max', 'array_min', 'custom_validate_cb' ], $valiate_cb->getValue( $field ) );
+		$expected = [
+			'label'      => 'Test Label',
+			'array_max'  => 10,
+			'array_min'  => 3,
+			'terms_args' => [ 'taxonomy' => 'question_tag', 'hide_empty' => false, 'fields' => 'id=>name' ],
+			'options'    => [
+				'option1' => 'Option 1',
+				'option2' => 'Option 2',
+				'option3' => 'Option 3',
+			],
+			'js_options' => [
+				'maxItems' => 10,
+				'form'     => 'Test Form',
+				'id'       => 'test-form',
+				'field'    => 'test-form',
+				'nonce'    => wp_create_nonce( 'tags_Test Formtest-form' ),
+				'create'   => false,
+				'labelAdd' => 'New Tag',
+			],
+			'sanitize'   => 'custom_sanitize_cb',
+			'validate'   => 'custom_validate_cb',
+		];
+		$this->assertEquals( $expected, $field->args );
+	}
+
+	/**
 	 * @covers AnsPress\Form\Field\Tags::sanitize_cb_args
 	 */
 	public function testSanitizeCbArgs() {
