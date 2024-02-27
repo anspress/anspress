e2b6d520a9503895721a6c477d1c0948c3c95769
Bishal Napit <mebishalnapit@gmail.com>
2024-02-27 12:22:53 +0545

    Fix - Autogrow implementation for the answer editor

--- templates/js/theme.js
+++ templates/js/theme.js
@@ -1,7 +1,11 @@
 (function ($) {
     $(document).ready(function () {
-        $('.wp-editor-area').autogrow({
-            onInitialize: true
+        $(document).on('focus', '.wp-editor-area', function() {
+            if (!$(this).data('autogrow_initialized')) {
+                $(this).autogrow({
+                    onInitialize: true
+                }).data('autogrow_initialized', true);
+            }
         });
 
         $('.ap-categories-list li .ap-icon-arrow-down').on('click', function (e) {
