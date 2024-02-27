e0437f059cc0d4b4dd7c24d0fa18a8e522c02455
Bishal Napit <mebishalnapit@gmail.com>
2024-02-27 15:07:15 +0545

    Add - PHPUnit test case for the anspress_verify_nonce function

--- tests/test-functions.php
+++ tests/test-functions.php
@@ -1647,6 +1647,22 @@ class TestFunctions extends TestCase {
 	}
 
 	/**
+	 * @covers ::anspress_verify_nonce
+	 */
+	public function testAnsPressVerifyNonce() {
+		// Test on invalid nonce.
+		$this->assertFalse( anspress_verify_nonce( 'anspress-tests' ) );
+		$this->assertFalse( anspress_verify_nonce( 'anspress-tests1' ) );
+
+		// Test on valid nonce.
+		$_REQUEST['__nonce'] = wp_create_nonce( 'anspress-tests' );
+		$this->assertIsInt( anspress_verify_nonce( 'anspress-tests' ) );
+		$_REQUEST['__nonce'] = wp_create_nonce( 'anspress-tests1' );
+		$this->assertIsInt( anspress_verify_nonce( 'anspress-tests1' ) );
+		unset( $_REQUEST['__nonce'] );
+	}
+
+	/**
 	 * @covers ::ap_canonical_url
 	 */
 	public function testap_canonical_url() {
