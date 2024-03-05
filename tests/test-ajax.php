aabc293410b3e1abc1c38981dba7ec8ff373f9c5
Bishal Napit <mebishalnapit@gmail.com>
2024-03-05 10:44:39 +0545

    Tweak - Move method exists check for AnsPress_Ajax class within its respective file

--- tests/test-ajax.php
+++ tests/test-ajax.php
@@ -22,21 +22,6 @@ class TestAjax extends TestCaseAjax {
 		);
 	}
 
-	public function testMethodExists() {
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'init' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'suggest_similar_questions' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'toggle_delete_post' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'permanent_delete_post' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'toggle_featured' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'close_question' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'send' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'load_tinymce' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'convert_to_post' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'load_filter_order_by' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'subscribe_to_question' ) );
-		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'search_tags' ) );
-	}
-
 	/**
 	 * @covers ::ap_send_json
 	 */
---
+++ tests/test-ajaxHooks.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Anspress\Tests;
+
+use AnsPress\WPTestUtils\WPIntegration\TestCaseAjax;
+
+class TestAjaxHooks extends TestCaseAjax {
+
+	use Testcases\Ajax;
+	use Testcases\Common;
+
+	public function testMethodExists() {
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'init' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'suggest_similar_questions' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'toggle_delete_post' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'permanent_delete_post' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'toggle_featured' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'close_question' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'send' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'load_tinymce' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'convert_to_post' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'load_filter_order_by' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'subscribe_to_question' ) );
+		$this->assertTrue( method_exists( 'AnsPress_Ajax', 'search_tags' ) );
+	}
+}
