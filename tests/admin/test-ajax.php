0503becd047e6c568062f81469eee1ca1e3bdb24
Bishal Napit <mebishalnapit@gmail.com>
2024-03-05 09:39:11 +0545

    Tweak - Small change

--- tests/admin/test-ajax.php
+++ tests/admin/test-ajax.php
@@ -556,9 +556,6 @@ class TestAdminAjax extends TestCaseAjax {
 		$this->_set_post_data( 'question_id=' . $question_id . '&ap_ajax_action=ap_get_all_answers' );
 		@$this->handle( 'ap_ajax' );
 		$response = json_decode( $this->_last_response, true );
-		$response = array_reverse( $response, true );
-		print_r( $response );
-		print_r( $answers );
 		foreach ( $response as $idx => $answer ) {
 			$this->assertEquals( $answers[ $idx ]['ID'], $answer['ID'] );
 			$this->assertEquals( $answers[ $idx ]['content'], $answer['content'] );
@@ -653,9 +650,6 @@ class TestAdminAjax extends TestCaseAjax {
 		$this->_set_post_data( 'question_id=' . $question_id . '&ap_ajax_action=ap_get_all_answers' );
 		@$this->handle( 'ap_ajax' );
 		$response = json_decode( $this->_last_response, true );
-		$response = array_reverse( $response, true );
-		print_r( $response );
-		print_r( $answers );
 		foreach ( $response as $idx => $answer ) {
 			$this->assertEquals( $answers[ $idx ]['ID'], $answer['ID'] );
 			$this->assertEquals( $answers[ $idx ]['content'], $answer['content'] );
