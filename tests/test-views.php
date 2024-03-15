db09ba928647937982066d3ce91ed3eaecdea3e4
Bishal Napit <mebishalnapit@gmail.com>
2024-03-15 11:43:42 +0545

    Fix - Code coverage for the AnsPress_Views::init method

--- tests/test-views.php
+++ tests/test-views.php
@@ -8,15 +8,19 @@ class TestViews extends TestCase {
 
 	use TestCases\Common;
 
-	public function testInit() {
-		$this->assertEquals( 10, has_action( 'shutdown', [ 'AnsPress_Views', 'insert_views' ] ) );
-		$this->assertEquals( 10, has_action( 'ap_before_delete_question', [ 'AnsPress_Vote', 'delete_votes' ] ) );
-	}
-
 	public function testMethodExists() {
 		$this->assertTrue( method_exists( 'AnsPress_Views', 'init' ) );
 		$this->assertTrue( method_exists( 'AnsPress_Views', 'insert_views' ) );
 		$this->assertTrue( method_exists( 'AnsPress_Views', 'delete_views' ) );
+	}
+
+	/**
+	 * @covers AnsPress_Views::init
+	 */
+	public function testInit() {
+		\AnsPress_Views::init();
+		$this->assertEquals( 10, has_action( 'shutdown', [ 'AnsPress_Views', 'insert_views' ] ) );
+		$this->assertEquals( 10, has_action( 'ap_before_delete_question', [ 'AnsPress_Vote', 'delete_votes' ] ) );
 	}
 
 	/**
