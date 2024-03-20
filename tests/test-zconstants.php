7a1d583d4a4dea7c85419b5c521df88851a5a441
Bishal Napit <mebishalnapit@gmail.com>
2024-03-20 15:37:41 +0545

    Add - PHPUnit test case for the ap_insert_reputation function with AP_DISABLE_INSERT_REP defined

--- tests/test-zconstants.php
+++ tests/test-zconstants.php
@@ -31,4 +31,29 @@ class TestZConstantsFunctionalities extends TestCase {
 		// Deactivate addon.
 		ap_deactivate_addon( 'notifications.php' );
 	}
+
+	/**
+	 * Test if user reputation is inserted if AP_DISABLE_INSERT_REP constant is defined.
+	 *
+	 * @covers ::ap_insert_reputation
+	 */
+	public function testAPInsertReputationShouldReturnNull() {
+		// Activate addon.
+		ap_activate_addon( 'reputation.php' );
+
+		// Before defining AP_DISABLE_INSERT_REP.
+		$this->assertFalse( defined( 'AP_DISABLE_INSERT_REP' ) );
+		$question_id = $this->insert_question();
+		$this->assertNotNull( ap_insert_reputation( 'ask', $question_id ) );
+
+		// After defining AP_DISABLE_INSERT_REP.
+		define( 'AP_DISABLE_INSERT_REP', true );
+		$this->assertTrue( defined( 'AP_DISABLE_INSERT_REP' ) );
+		$this->setRole( 'subscriber' );
+		$question_id = $this->insert_question();
+		$this->assertNull( ap_insert_reputation( 'ask', $question_id ) );
+
+		// Deactivate addon.
+		ap_deactivate_addon( 'reputation.php' );
+	}
 }
