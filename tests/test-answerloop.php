16cc62a15345f3027197ed737f4310c330a32860
Bishal Napit <mebishalnapit@gmail.com>
2024-03-29 14:10:59 +0545

    Tweak - Answer_Query unit tests within its own file

--- tests/test-answerloop.php
+++ tests/test-answerloop.php
@@ -8,25 +8,6 @@ class TestAnswerLoop extends TestCase {
 
 	use TestCases\Common;
 
-	public function testClassProperties() {
-		$class = new \ReflectionClass( 'Answers_Query' );
-		$this->assertTrue( $class->hasProperty( 'args' ) && $class->getProperty( 'args' )->isPublic() );
-	}
-
-	public function testMethodExists() {
-		$this->assertTrue( method_exists( 'Answers_Query', '__construct' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'get_answers' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'next_answer' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'reset_next' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'the_answer' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'have_answers' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'rewind_answers' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'is_main_query' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'reset_answers_data' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'get_ids' ) );
-		$this->assertTrue( method_exists( 'Answers_Query', 'pre_fetch' ) );
-	}
-
 	/**
 	 * @covers ::ap_answer_status
 	 */
---
+++ tests/test-answersQuery.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Anspress\Tests;
+
+use Yoast\WPTestUtils\WPIntegration\TestCase;
+
+class TestAnswersQuery extends TestCase {
+
+	public function testClassProperties() {
+		$class = new \ReflectionClass( 'Answers_Query' );
+		$this->assertTrue( $class->hasProperty( 'args' ) && $class->getProperty( 'args' )->isPublic() );
+	}
+
+	public function testMethodExists() {
+		$this->assertTrue( method_exists( 'Answers_Query', '__construct' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'get_answers' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'next_answer' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'reset_next' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'the_answer' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'have_answers' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'rewind_answers' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'is_main_query' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'reset_answers_data' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'get_ids' ) );
+		$this->assertTrue( method_exists( 'Answers_Query', 'pre_fetch' ) );
+	}
+}
