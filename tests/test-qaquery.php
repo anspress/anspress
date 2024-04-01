b42c8a8bcf65bbe8bcbddc417dcec9a11ff41858
Bishal Napit <mebishalnapit@gmail.com>
2024-04-01 14:58:03 +0545

    Tweak - PHPUnit test for Question_Query class within a single file

--- tests/test-qaquery.php
+++ tests/test-qaquery.php
@@ -20,25 +20,6 @@ class TestQAQuery extends TestCase {
 		parent::tear_down();
 	}
 
-	public function testClassProperties() {
-		$class = new \ReflectionClass( 'Question_Query' );
-		$this->assertTrue( $class->hasProperty( 'count_request' ) && $class->getProperty( 'count_request' )->isPublic() );
-	}
-
-	public function testMethodExists() {
-		$this->assertTrue( method_exists( 'Question_Query', '__construct' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'get_questions' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'next_question' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'reset_next' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'the_question' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'have_questions' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'rewind_questions' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'is_main_query' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'reset_questions_data' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'get_ids' ) );
-		$this->assertTrue( method_exists( 'Question_Query', 'pre_fetch' ) );
-	}
-
 	/**
 	 * @covers ::ap_question_status
 	 */
---
+++ tests/test-questionsQuery.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Anspress\Tests;
+
+use Yoast\WPTestUtils\WPIntegration\TestCase;
+
+class TestQuestionsQuery extends TestCase {
+
+	use Testcases\Common;
+
+	public function testClassProperties() {
+		$class = new \ReflectionClass( 'Question_Query' );
+		$this->assertTrue( $class->hasProperty( 'count_request' ) && $class->getProperty( 'count_request' )->isPublic() );
+	}
+
+	public function testMethodExists() {
+		$this->assertTrue( method_exists( 'Question_Query', '__construct' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'get_questions' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'next_question' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'reset_next' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'the_question' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'have_questions' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'rewind_questions' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'is_main_query' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'reset_questions_data' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'get_ids' ) );
+		$this->assertTrue( method_exists( 'Question_Query', 'pre_fetch' ) );
+	}
+}
