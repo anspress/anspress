bd18b18b7e59026848c7d45d9c7b3113ecf42e45
Bishal Napit <mebishalnapit@gmail.com>
2024-04-11 10:24:48 +0545

    Add - PHPUnit test case for the method visibility of the \AnsPress\Singleton class

--- tests/test-singleton.php
+++ tests/test-singleton.php
@@ -12,4 +12,19 @@ class TestSingleton extends TestCase {
 		$this->assertTrue( method_exists( '\AnsPress\Singleton', '__wakeup' ) );
 		$this->assertTrue( method_exists( '\AnsPress\Singleton', 'run_once' ) );
 	}
+
+	public function testMethodVisibility() {
+		$reflection = new \ReflectionClass( '\AnsPress\Singleton' );
+		$method = $reflection->getMethod( 'init' );
+		$this->assertTrue( $method->isPublic() );
+
+		$method = $reflection->getMethod( '__clone' );
+		$this->assertTrue( $method->isPrivate() );
+
+		$method = $reflection->getMethod( '__wakeup' );
+		$this->assertTrue( $method->isPublic() );
+
+		$method = $reflection->getMethod( 'run_once' );
+		$this->assertTrue( $method->isPublic() );
+	}
 }
