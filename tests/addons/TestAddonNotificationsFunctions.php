4f8faedc1b92d3c13a2e01d1738780464b0954c5
Rahul Arya <rah12@live.com>
2024-04-13 06:17:53 +0000

    Mark test as incomplete in TestAddonNotificationsFunctions.php

--- tests/addons/TestAddonNotificationsFunctions.php
+++ tests/addons/TestAddonNotificationsFunctions.php
@@ -9,6 +9,7 @@ class TestAddonNotificationsFunctions extends TestCase {
 	use Testcases\Common;
 
 	public function set_up() {
+		$this->markTestIncomplete();
 		parent::set_up();
 		ap_activate_addon( 'notifications.php' );
 		\AnsPress\Addons\ap_notification_addon_activation();
