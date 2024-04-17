69e6502400b9fc2d1f75988efe764340357428b2
Bishal Napit <mebishalnapit@gmail.com>
2024-04-17 13:11:59 +0545

    Add - PHPUnit test case for the Anspress\Addons\Notifications::mark_notifications_seen method

--- tests/addons/test-ajaxAddons.php
+++ tests/addons/test-ajaxAddons.php
@@ -12,11 +12,13 @@ class TestAjaxAddons extends TestCaseAjax {
 	public function set_up() {
 		parent::set_up();
 		ap_activate_addon( 'reputation.php' );
+		ap_activate_addon( 'notifications.php' );
 	}
 
 	public function tear_down() {
 		parent::tear_down();
 		ap_deactivate_addon( 'reputation.php' );
+		ap_deactivate_addon( 'notifications.php' );
 	}
 
 	/**
@@ -77,5 +79,77 @@ class TestAjaxAddons extends TestCaseAjax {
 		$get_option = get_option( 'anspress_reputation_events' );
 		$this->assertNotEmpty( $get_option );
 		$this->assertEquals( [ 'test_event' => 12 ], $get_option );
+	}
+
+	/**
+	 * @covers Anspress\Addons\Notifications::mark_notifications_seen
+	 */
+	public function testMarkNotificationsSeenForNonLoggedInUsers() {
+		$instance = \Anspress\Addons\Notifications::init();
+		add_action( 'ap_ajax_mark_notifications_seen', [ $instance, 'mark_notifications_seen' ] );
+
+		// Insert some dummy notifications.
+		$user_id = $this->factory()->user->create();
+		wp_set_current_user( $user_id );
+		ap_insert_notification();
+		$noti_seen = ap_get_notifications( [] );
+		$this->assertEquals( 0, $noti_seen[0]->noti_seen );
+		$this->logout();
+
+		// Test.
+		$this->_set_post_data( 'ap_ajax_action=mark_notifications_seen&__nonce=' . wp_create_nonce( 'mark_notifications_seen' ) );
+		$this->handle( 'ap_ajax' );
+		wp_set_current_user( $user_id );
+		$noti_seen = ap_get_notifications( [] );
+		$this->assertEquals( 0, $noti_seen[0]->noti_seen );
+		$this->assertFalse( $this->ap_ajax_success( 'success' ) );
+		$this->assertTrue( $this->ap_ajax_success( 'snackbar' )->message === 'There was a problem processing your request' );
+	}
+
+	/**
+	 * @covers Anspress\Addons\Notifications::mark_notifications_seen
+	 */
+	public function testMarkNotificationsSeenForInvalidNonce() {
+		$instance = \Anspress\Addons\Notifications::init();
+		add_action( 'ap_ajax_mark_notifications_seen', [ $instance, 'mark_notifications_seen' ] );
+
+		// Insert some dummy notifications.
+		$this->setRole( 'subscriber' );
+		ap_insert_notification();
+		$noti_seen = ap_get_notifications( [] );
+		$this->assertEquals( 0, $noti_seen[0]->noti_seen );
+
+		// Test.
+		$this->_set_post_data( 'ap_ajax_action=mark_notifications_seen&__nonce=' . wp_create_nonce( 'invalid_nonce' ) );
+		$this->handle( 'ap_ajax' );
+		$noti_seen = ap_get_notifications( [] );
+		$this->assertEquals( 0, $noti_seen[0]->noti_seen );
+		$this->assertFalse( $this->ap_ajax_success( 'success' ) );
+		$this->assertTrue( $this->ap_ajax_success( 'snackbar' )->message === 'There was a problem processing your request' );
+
+	}
+
+	/**
+	 * @covers Anspress\Addons\Notifications::mark_notifications_seen
+	 */
+	public function testMarkNotificationsSeen() {
+		$instance = \Anspress\Addons\Notifications::init();
+		add_action( 'ap_ajax_mark_notifications_seen', [ $instance, 'mark_notifications_seen' ] );
+
+		// Insert some dummy notifications.
+		$this->setRole( 'subscriber' );
+		ap_insert_notification();
+		$noti_seen = ap_get_notifications( [] );
+		$this->assertEquals( 0, $noti_seen[0]->noti_seen );
+
+		// Test.
+		$this->_set_post_data( 'ap_ajax_action=mark_notifications_seen&__nonce=' . wp_create_nonce( 'mark_notifications_seen' ) );
+		$this->handle( 'ap_ajax' );
+		$noti_seen = ap_get_notifications( [] );
+		$this->assertEquals( 1, $noti_seen[0]->noti_seen );
+		$this->assertTrue( $this->ap_ajax_success( 'success' ) );
+		$this->assertTrue( $this->ap_ajax_success( 'btn' )->hide === true );
+		$this->assertTrue( $this->ap_ajax_success( 'snackbar' )->message === 'Successfully updated all notifications' );
+		$this->assertTrue( $this->ap_ajax_success( 'cb' ) === 'notificationAllRead' );
 	}
 }
