670c063738203784432e930df28d2bb5db1c4be1
Rahul Arya <rah12@live.com>
2024-06-10 06:11:18 +0000

    Update auth policies

---
+++ src/backend/Modules/Vote/VoteModule.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The vote module.
+ *
+ * @package AnsPress
+ * @since 5.0.0
+ */
+
+namespace AnsPress\Modules\Vote;
+
+use AnsPress\Classes\AbstractModule;
+use AnsPress\Classes\Plugin;
+
+/**
+ * Category module class.
+ *
+ * @since 5.0.0
+ */
+class VoteModule extends AbstractModule {
+	/**
+	 * Register hooks.
+	 *
+	 * @return void
+	 */
+	public function register_hooks() {
+	}
+}
---
+++ src/backend/Modules/Vote/VoteService.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Vote service.
+ *
+ * @since 5.0.0
+ * @package AnsPress
+ */
+
+namespace AnsPress\Modules\Vote;
+
+use AnsPress\Classes\AbstractService;
+use AnsPress\Classes\Auth;
+use AnsPress\Classes\Validator;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Vote service.
+ *
+ * @since 5.0.0
+ */
+class VoteService extends AbstractService {
+	/**
+	 * Create a new vote.
+	 *
+	 * @param array $data Vote data.
+	 * @return null|VoteModel  Vote model.
+	 */
+	public function create( array $data ): ?VoteModel {
+		// Check if user can create vote.
+		Auth::checkAndThrow( 'create', new VoteModel() );
+
+		$data['vote_user_id'] = get_current_user_id();
+
+		$validator = new Validator(
+			$data,
+			array(
+				'vote_user_id' => 'required|numeric|exists:users,ID',
+				'vote_event'   => 'required|string',
+				'vote_ref_id'  => 'required|numeric',
+			)
+		);
+
+		$validated = $validator->validated();
+
+		$vote = new VoteModel();
+
+		$vote->fill( $validated );
+
+		$updated = $vote->save();
+
+		return $updated;
+	}
+}
